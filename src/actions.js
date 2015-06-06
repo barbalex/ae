4b03e24dbf11332b0a1e3448de5181421ddee4c8
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-06-06 23:52:46 +0200

    drafted actions

--- src/actions.js
+++ src/actions.js
@@ -1,9 +1,14 @@
 'use strict'
 
 import Reflux from 'reflux'
+import app from 'ampersand-app'
 
   // Each action is like an event channel for one specific event. Actions are called by components.
   // The store is listening to all actions, and the components in turn are listening to the store.
   // Thus the flow is: User interaction -> component calls action -> store reacts and triggers -> components update
 
-  
+export default function () {
+  app.Actions = Reflux.createActions([
+    showObjectForm
+  ])
+}
