f37a42f7aead5c2eabf72b7af8e00a842d9425bf
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-06-07 21:24:38 +0200

    actions extended in app

--- src/app.js
+++ src/app.js
@@ -3,12 +3,14 @@
 import app from 'ampersand-app'
 import './styles/main.styl'
 import Router from './router.js'
+import Actions from './actions'
 
 window.app = app // expose app to the browser console
 
 app.extend({
   init () {
     this.router = new Router()
+    this.Actions = Actions()
   }
 })
 
