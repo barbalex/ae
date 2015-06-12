4601224121ab488bf03e42fb8725eaff9e07de4b
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-06-12 16:52:14 +0200

    Revert "starting router better"
    This reverts commit c57f677c5a5d22b66a792beb6e19f1f3f0f0a230.

--- src/router.js
+++ src/router.js
@@ -1,14 +1,54 @@
 'use strict'
 
-// import app from 'ampersand-app'
+import app from 'ampersand-app'
 import React from 'react'
 import Router from 'react-router'
-import routes from './routes.js'
 
-console.log('router: routes:', routes())
+import Favicon from 'react-favicon'
+import MenuButton from './components/menuButton'
+import Menu from './components/menu/menu.js'
+import Objekt from './components/main/object/object.js'
+import FourOhFour from './components/main/fourOhFour.js'
+import Empty from './components/main/empty.js'
+import S1 from './components/menu/tree/s1.js'
+import S2 from './components/menu/tree/s2.js'
+import S3 from './components/menu/tree/s3.js'
+import S4 from './components/menu/tree/s4.js'
+import FaviconImage from '../img/aster_144.png'
 
-// return all the router methods
-// to build an instance in app
+const DefaultRoute = Router.DefaultRoute
+const NotFoundRoute = Router.NotFoundRoute
+const Route = Router.Route
+const RouteHandler = Router.RouteHandler
+
+const Apphandler = React.createClass({
+  displayName: 'Apphandler',
+
+  render () {
+    return (
+      <div>
+        <Favicon url={[FaviconImage]}/>
+        <MenuButton/>
+        <Menu/>
+        {/*<Objekt/>*/}
+        <RouteHandler/>
+      </div>
+    )
+  }
+})
+
+const routes = (
+  <Route name='start' path='/' handler={Apphandler}>
+    <Route name='s1' path='/:s1' handler={S1}/>
+    <Route name='s2' path='/:s1/:s2' handler={S2}/>
+    <Route name='s3' path='/:s1/:s2/:s3' handler={S3}/>
+    <Route name='s4' path='/:s1/:s2/:s3/:s4' handler={S4}/>
+    <Route name='s5' path='/:s1/:s2/:s3/:s4/:s5' handler={Objekt}/>
+    <DefaultRoute handler={Empty}/>
+    <NotFoundRoute handler={FourOhFour}/>
+  </Route>
+)
+
 export default {
   getCurrentPath () {
     return router.getCurrentPath()
@@ -42,7 +82,7 @@ export default {
 }
 
 const router = Router.create({
-  routes: routes(),
+  routes: routes,
   location: Router.HistoryLocation
 })
 
