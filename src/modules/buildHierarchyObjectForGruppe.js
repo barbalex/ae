c5848e7f4b8b1da908e4dab63d501b166b1abd3f
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-07-12 01:41:46 +0200

    sync

--- src/modules/buildHierarchyObjectForGruppe.js
+++ src/modules/buildHierarchyObjectForGruppe.js
@@ -3,40 +3,31 @@
  * GUID's mark that this object can be shown
  * example:
     {
-      'Fauna': {
-        'name': 'Fauna',
-        'children': {
-          'Amphibia': {
-            'name': 'Amphibia',
-            'children': {
-              'Anura': {
-                'name': 'Anura',
-                'children': {
-                  'Bufonidae': {
-                    'name': 'Bufonidae',
-                    'children': {
-                      'Bufo bufo (Linnaeus, 1758) (Erdkröte)': {
-                        'name': 'Bufo bufo (Linnaeus, 1758) (Erdkröte)',
-                        'guid': '979233B6-9013-4820-9F7D-8ED9D826C2D3'
-                      }
-                      ...
+      'name': 'Fauna',
+      'children': [
+        {
+          'name': 'Amphibia',
+          'children': [
+            {
+              'name': 'Anura',
+              'children': [
+                {
+                  'name': 'Bufonidae',
+                  'children': [
+                    {
+                      'name': 'Bufo bufo (Linnaeus, 1758) (Erdkröte)',
+                      'guid': '979233B6-9013-4820-9F7D-8ED9D826C2D3'
                     }
-                  }
-                  ...
+                  ]
                 }
-              }
-              ...
+              ]
             }
-          }
-          ...
+          ]
         }
-      }
-      'Flora': ...
-      'Moose': ...
-      ...
+      ]
     }
  * the function recieves the objects of a group
- * it builds app.hierarchieObject[gruppe]
+ * it builds hierarchy
  * building principle:
  * 0. use the array in Eigenschaften.Hierarchie of the desired taxonomy
  * 1. build first level (first element)
@@ -53,97 +44,66 @@ import app from 'ampersand-app'
 import _ from 'lodash'
 import passPropertyToHierarchieObject from './passPropertyToHierarchieObject.js'
 
-function buildNextLevel (name, path, hierarchy, gruppe) {
-  if (path.length > 3) return
-
-  console.log('buildNextLevel name', name)
-  console.log('buildNextLevel path', path)
-  console.log('buildNextLevel hierarchy', hierarchy)
-
-  const hierarchiesWithName = _.filter(hierarchy, function (hierarchyArray) {
-    return hierarchyArray[0] && hierarchyArray[0].Name
+function checkLevel1 (hierarchy, hArray) {
+  const el1 = _.find(hierarchy, function (el) {
+    return el.name === hArray[0].name
   })
-  const hierarchies = _.groupBy(hierarchiesWithName, function (hierarchyArray) {
-    return hierarchyArray[0].Name
-  })
-
-  if (hierarchy.length > 1) {
-
-    console.log('buildNextLevel, hierarchies', hierarchies)
-
-    if (hierarchies) {
-      passPropertyToHierarchieObject(hierarchy.GUID, hierarchies, path, name, gruppe)
-      _.forEach(hierarchies, function (propHierarchy, propName) {
-        if (propName) {
-          let nextPath = _.clone(path)
-          nextPath.push(propName)
-          buildNextLevel(propName, nextPath, propHierarchy)
-        }
-      })
-    }
-  } else {
-    // this is last level
-    passPropertyToHierarchieObject(hierarchy.GUID, hierarchies, path, name, gruppe)
-  }
+  if (!el1) hierarchy.push(hArray[0])
 }
 
-function structurize (hierarchiesArray) {
-  let items = []
-  const l = hierarchiesArray.length
-  for (let i = 0; i < l; i++) {
-    const hierarchy = hierarchiesArray[i]
-    const name = hierarchy[0].Name
-    const guid = hierarchy[0].GUID
-    const rest = hierarchy.slice(1)
-    let item = null
-    const itemsLength = items.length
-    for (let j = 0; j < itemsLength; j++) {
-      if (items[j].name === name) {
-        item = items[j]
-        break
-      }
-    }
-    if (item === null) {
-      item = {}
-      item.name = name
-      if (guid) item.guid = guid
-      if (rest.length > 0) {
-        item.children = [rest]
-      } else {
-        item.children = []
-      }
-      item.children = rest.length > 0 ? [rest] : []
-      items.push(item)
-    }
-  }
-  return items
+function checkLevel2 (hierarchy, hArray) {
+  checkLevel1(hierarchy, hArray)
+  const el1 = _.find(hierarchy, function (el) {
+    return el.name === hArray[0].name
+  })
+
+  const el2 = _.find(el1.children, function (el) {
+    return el.name === hArray[1].name
+  })
+  if (!el2) hierarchy.push(hArray[1])
 }
 
 export default function (objects, gruppe) {
   // prepare hierarchieObject
-  app.hierarchieObject = app.hierarchieObject || {}
-  // app.hierarchieObject[gruppe] = {}
+  let hierarchy = []
 
   // extract Hierarchie from objects
   const hierarchiesArray = _.map(objects, function (object) {
     if (object.Taxonomien[0].Eigenschaften.Hierarchie) return _.get(object, 'Taxonomien[0].Eigenschaften.Hierarchie')
   })
 
-  const items = structurize(hierarchiesArray)
-  console.log('buildHierarchyObjectForGruppe.js: items', items)
-  /*const l = items.length
-  for (let i = 0; i < l; i++) {
-    let item = items[i]
-    item.children = structurize(item.children)
-  }*/
-  console.log('buildHierarchyObjectForGruppe.js: items after adding children', items)
-
-  const gruppeItem = {
-    name: gruppe,
-    children: items
-  }
-
-  console.log('buildHierarchyObjectForGruppe.js: gruppeItem', gruppeItem)
+  _.forEach(hierarchiesArray, function (hArray) {
+    let el1, el2, el3, el4, el5, el6, el7
+    switch (hArray.length) {
+    case 1:
+      checkLevel1(hierarchy, hArray)
+      break
+    case 2:
+      checkLevel2(hierarchy, hArray)
+      break
+    /*case 3:
+      hierarchy.children[hArray[1]].children[hArray[2]] = objectToPass
+      break
+    case 4:
+      hierarchy.children[hArray[1]].children[hArray[2]].children[hArray[3]].children = objectToPass
+      break
+    case 5:
+      hierarchy.children[hArray[1]].children[hArray[2]].children[hArray[3]].children[hArray[4]].children = objectToPass
+      break
+    case 6:
+      hierarchy.children[hArray[1]].children[hArray[2]].children[hArray[3]].children[hArray[4]].children[hArray[5]].children = objectToPass
+      break
+    case 7:
+      hierarchy.children[hArray[1]].children[hArray[2]].children[hArray[3]].children[hArray[4]].children[hArray[5]].children[hArray[6]].children = objectToPass
+      break
+    case 8:
+      hierarchy.children[hArray[1]].children[hArray[2]].children[hArray[3]].children[hArray[4]].children[hArray[5]].children[hArray[6]].children[hArray[7]].children = objectToPass
+      break
+    case 9:
+      hierarchy.children[hArray[1]].children[hArray[2]].children[hArray[3]].children[hArray[4]].children[hArray[5]].children[hArray[6]].children[hArray[7]].children[hArray[8]].children = objectToPass
+      break*/
+    }
+  })
 
   return gruppeItem
 }
--- src/modules/buildHierarchyObjectForGruppe_old.js
+++ src/modules/buildHierarchyObjectForGruppe_old.js
@@ -3,37 +3,28 @@
  * GUID's mark that this object can be shown
  * example:
     {
-      'Fauna': {
-        'name': 'Fauna',
-        'children': {
-          'Amphibia': {
-            'name': 'Amphibia',
-            'children': {
-              'Anura': {
-                'name': 'Anura',
-                'children': {
-                  'Bufonidae': {
-                    'name': 'Bufonidae',
-                    'children': {
-                      'Bufo bufo (Linnaeus, 1758) (Erdkröte)': {
-                        'name': 'Bufo bufo (Linnaeus, 1758) (Erdkröte)',
-                        'guid': '979233B6-9013-4820-9F7D-8ED9D826C2D3'
-                      }
-                      ...
+      'name': 'Fauna',
+      'children': [
+        {
+          'name': 'Amphibia',
+          'children': [
+            {
+              'name': 'Anura',
+              'children': [
+                {
+                  'name': 'Bufonidae',
+                  'children': [
+                    {
+                      'name': 'Bufo bufo (Linnaeus, 1758) (Erdkröte)',
+                      'guid': '979233B6-9013-4820-9F7D-8ED9D826C2D3'
                     }
-                  }
-                  ...
+                  ]
                 }
-              }
-              ...
+              ]
             }
-          }
-          ...
+          ]
         }
-      }
-      'Flora': ...
-      'Moose': ...
-      ...
+      ]
     }
  * the function recieves the objects of a group
  * it builds app.hierarchieObject[gruppe]
@@ -53,38 +44,36 @@ import app from 'ampersand-app'
 import _ from 'lodash'
 import passPropertyToHierarchieObject from './passPropertyToHierarchieObject.js'
 
-function buildNextLevel (name, path, hierarchy, gruppe) {
-  if (path.length > 3) return
-
-  console.log('buildNextLevel name', name)
-  console.log('buildNextLevel path', path)
-  console.log('buildNextLevel hierarchy', hierarchy)
-
-  const hierarchiesWithName = _.filter(hierarchy, function (hierarchyArray) {
-    return hierarchyArray[0] && hierarchyArray[0].Name
-  })
-  const hierarchies = _.groupBy(hierarchiesWithName, function (hierarchyArray) {
-    return hierarchyArray[0].Name
-  })
-
-  if (hierarchy.length > 1) {
-
-    console.log('buildNextLevel, hierarchies', hierarchies)
-
-    if (hierarchies) {
-      passPropertyToHierarchieObject(hierarchy.GUID, hierarchies, path, name, gruppe)
-      _.forEach(hierarchies, function (propHierarchy, propName) {
-        if (propName) {
-          let nextPath = _.clone(path)
-          nextPath.push(propName)
-          buildNextLevel(propName, nextPath, propHierarchy)
-        }
-      })
+function structurize (hierarchiesArray) {
+  let items = []
+  const l = hierarchiesArray.length
+  for (let i = 0; i < l; i++) {
+    const hierarchy = hierarchiesArray[i]
+    const name = hierarchy[0].Name
+    const guid = hierarchy[0].GUID
+    const rest = hierarchy.slice(1)
+    let item = null
+    const itemsLength = items.length
+    for (let j = 0; j < itemsLength; j++) {
+      if (items[j].name === name) {
+        item = items[j]
+        break
+      }
     }
-  } else {
-    // this is last level
-    passPropertyToHierarchieObject(hierarchy.GUID, hierarchies, path, name, gruppe)
+    if (item === null) {
+      item = {}
+      item.name = name
+      if (guid) item.guid = guid
+      if (rest.length > 0) {
+        item.children = [rest]
+      } else {
+        item.children = []
+      }
+      item.children = rest.length > 0 ? [rest] : []
+      items.push(item)
+    }
   }
+  return items
 }
 
 export default function (objects, gruppe) {
@@ -96,24 +85,22 @@ export default function (objects, gruppe) {
   const hierarchiesArray = _.map(objects, function (object) {
     if (object.Taxonomien[0].Eigenschaften.Hierarchie) return _.get(object, 'Taxonomien[0].Eigenschaften.Hierarchie')
   })
-  // console.log('buildHierarchyObjectForGruppe.js hierarchiesArray', hierarchiesArray)
-  const hierarchies = _.groupBy(hierarchiesArray, function (hierarchyArray) {
-    if (hierarchyArray[0] && hierarchyArray[0].Name) return hierarchyArray[0].Name
-  })
 
-  passPropertyToHierarchieObject(null, hierarchies, [gruppe], gruppe, gruppe)
+  const items = structurize(hierarchiesArray)
+  console.log('buildHierarchyObjectForGruppe.js: items', items)
+  /*const l = items.length
+  for (let i = 0; i < l; i++) {
+    let item = items[i]
+    item.children = structurize(item.children)
+  }*/
+  console.log('buildHierarchyObjectForGruppe.js: items after adding children', items)
 
-  // console.log('buildHierarchyObjectForGruppe.js hierarchies', hierarchies)
-  // console.log('buildHierarchyObjectForGruppe.js objectToPass', objectToPass)
+  const gruppeItem = {
+    name: gruppe,
+    children: items
+  }
 
-  // build next level
-  _.forEach(hierarchies, function (hierarchy, name) {
-    const path = [gruppe, name]
-    console.log('buildHierarchyObjectForGruppe.js hierarchy', hierarchy)
-    console.log('buildHierarchyObjectForGruppe.js name', name)
-    console.log('buildHierarchyObjectForGruppe.js path', path)
-    buildNextLevel(name, path, hierarchy, gruppe)
-  })
+  console.log('buildHierarchyObjectForGruppe.js: gruppeItem', gruppeItem)
 
-  return app.hierarchieObject[gruppe]
+  return gruppeItem
 }
