b4425cbfbff95d0b1301b315d6cf21294d7adce0
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-01 10:20:20 +0200

    modularize nutzungsbedingungen

--- src/components/main/importPc/importPc.js
+++ src/components/main/importPc/importPc.js
@@ -14,6 +14,7 @@ import WellAnforderungenInhaltlich from './wellAnforderungenInhaltlich.js'
 import SelectNameBestehend from './selectNameBestehend.js'
 import InputBeschreibung from './inputBeschreibung.js'
 import InputDatenstand from './inputDatenstand.js'
+import InputNutzungsbedingungen from './inputNutzungsbedingungen.js'
 import SelectUrsprungsEs from './selectUrsprungsEs.js'
 import AlertIdsAnalysisResult from './alertIdsAnalysisResult.js'
 import TablePreview from './tablePreview.js'
@@ -160,11 +161,8 @@ export default React.createClass({
     this.setState({ datenstand: datenstand })
   },
 
-  onChangeNutzungsbedingungen (event) {
-    const nutzungsbedingungen = event.target.value
-    this.setState({
-      nutzungsbedingungen: nutzungsbedingungen
-    })
+  onChangeNutzungsbedingungen (nutzungsbedingungen) {
+    this.setState({ nutzungsbedingungen: nutzungsbedingungen })
   },
 
   onChangeLink (event) {
@@ -423,20 +421,6 @@ export default React.createClass({
     )
   },
 
-  nutzungsbedingungenPopover () {
-    return (
-      <Popover title='Wozu Nutzunsbedingungen?'>
-        <p>Der Nutzer soll wissen, was er mit den Daten machen darf.</p>
-        <p><strong>Beispiele:</strong></p>
-        <p>Wenn <strong>fremde Daten</strong> mit Einverständnis des Autors importiert werden:<br/>
-        "Importiert mit Einverständnis des Autors. Eine allfällige Weiterverbreitung ist nur mit dessen Zustimmung möglich."</p>
-        <p>Wenn <strong>eigene Daten</strong> importiert werden:<br/>
-        "Open Data: Die veröffentlichten Daten dürfen mit Hinweis auf die Quelle vervielfältigt, verbreitet und weiter zugänglich gemacht, angereichert und bearbeitet sowie kommerziell genutzt werden. Für die Richtigkeit, Genauigkeit, Zuverlässigkeit und Vollständigkeit der bezogenen, ebenso wie der daraus erzeugten Daten und anderer mit Hilfe dieser Daten hergestellten Produkte wird indessen keine Haftung übernommen."</p>
-        <p><em>Tipp: Klicken Sie auf "Nutzungsbedingungen". Dann bleibt diese Meldung offen und Sie können den Beispieltext kopieren.</em></p>
-      </Popover>
-    )
-  },
-
   linkPopover () {
     return (
       <Popover title='Wozu ein Link?'>
@@ -502,15 +486,7 @@ export default React.createClass({
 
             <InputDatenstand datenstand={datenstand} validDatenstand={validDatenstand} onChangeDatenstand={this.onChangeDatenstand} />
 
-            <div className={validNutzungsbedingungen ? 'form-group' : 'form-group has-error'}>
-              <OverlayTrigger trigger='click' rootClose placement='right' overlay={this.nutzungsbedingungenPopover()}>
-                <OverlayTrigger trigger={['hover', 'focus']} placement='right' overlay={this.nutzungsbedingungenPopover()}>
-                  <label className='control-label withPopover'>Nutzungsbedingungen</label>
-                </OverlayTrigger>
-              </OverlayTrigger>
-              <textarea className='form-control controls' rows={1} value={nutzungsbedingungen} onChange={this.onChangeNutzungsbedingungen}></textarea>
-              {validNutzungsbedingungen ? null : <div className='validateDiv feld'>Nutzungsbedingungen sind erforderlich</div>}
-            </div>
+            <InputNutzungsbedingungen nutzungsbedingungen={nutzungsbedingungen} validNutzungsbedingungen={validNutzungsbedingungen} onChangeNutzungsbedingungen={this.onChangeNutzungsbedingungen} />
 
             <div className={validLink ? 'form-group' : 'form-group has-error'}>
               <OverlayTrigger trigger='click' rootClose placement='right' overlay={this.linkPopover()}>
