98eab0b227c567aad304df9bb97af6c4d04c5679
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-01 21:08:35 +0200

    optimize code

--- src/modules/getItemsById.js
+++ src/modules/getItemsById.js
@@ -1,3 +1,9 @@
+/*
+ * built for the import
+ * idField is values of Field "Zugehörige ID in ArtenDb"
+ * it is one of: GUID, Fauna, Flora, Moose, Macromycetes
+ */
+
 'use strict'
 
 import app from 'ampersand-app'
@@ -10,36 +16,35 @@ import queryMacromycetes from '../queries/macromycetesById.js'
 export default function (idField, ids) {
   // build object of functions, to call dynamically
   let dynamicFuntions = {
+    GUID: function (ids) {
+      return new Promise(function (resolve, reject) {
+        const options = {
+          keys: ids,
+          include_docs: true
+        }
+        app.localDb.allDocs(options)
+          .then(function (result) {
+            const docs = _.pluck(result.rows, 'doc')
+            resolve(docs)
+          })
+          .catch(function (error) {
+            reject('error fetching docs', error)
+          })
+      })
+    },
     Fauna: queryFauna,
     Flora: queryFlora,
     Moose: queryMoose,
     Macromycetes: queryMacromycetes
   }
   return new Promise(function (resolve, reject) {
-    // call the apropriate view and pass the ids
-    // by taxonomie id: viewname = Gruppe.toLowerCase() + ById
-    // guid: allDocs
-    if (idField === 'GUID') {
-      const options = {
-        keys: ids,
-        include_docs: true
-      }
-      app.localDb.allDocs(options)
-        .then(function (result) {
-          const docs = _.pluck(result.rows, 'doc')
-          resolve(docs)
-        })
-        .catch(function (error) {
-          reject('error fetching docs', error)
-        })
-    } else {
-      dynamicFuntions[idField](ids)
-        .then(function (docs) {
-          resolve(docs)
-        })
-        .catch(function (error) {
-          reject('error fetching docs', error)
-        })
-    }
+    // call the apropriate function and pass the ids
+    dynamicFuntions[idField](ids)
+      .then(function (docs) {
+        resolve(docs)
+      })
+      .catch(function (error) {
+        reject('error fetching docs', error)
+      })
   })
 }
