f039f0ebf3b415316963a1d94bad94a2c9cff82f
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-03 14:07:06 +0200

    use es6 templates

--- src/modules/buildGoogleImageLink.js
+++ src/modules/buildGoogleImageLink.js
@@ -3,48 +3,34 @@
 export default (object) => {
   if (object && object.Taxonomie && object.Taxonomie.Eigenschaften) {
     const properties = object.Taxonomie.Eigenschaften
-    let link = ''
+    const { Artname, Gattung, Art, Name, Einheit } = properties
+    let link = 'https://www.google.ch/search?tbm=isch&q='
 
     switch (object.Gruppe) {
       case 'Flora':
-        link = 'https://www.google.ch/search?num=10&hl=de&site=imghp&tbm=isch&source=hp&bih=824&q="' + properties.Artname + '"'
-        if (properties['Name Deutsch']) {
-          link += '+OR+"' + properties['Name Deutsch'] + '"'
-        }
-        if (properties['Name Französisch']) {
-          link += '+OR+"' + properties['Name Französisch'] + '"'
-        }
-        if (properties['Name Italienisch']) {
-          link += '+OR+"' + properties['Name Italienisch'] + '"'
-        }
+        link += `"${Artname}"`
+        if (properties['Name Deutsch']) link += ` OR "${properties['Name Deutsch']}"`
+        if (properties['Name Französisch']) link += ` OR "${properties['Name Französisch']}"`
+        if (properties['Name Italienisch']) link += ` OR "${properties['Name Italienisch']}"`
         break
       case 'Fauna':
-        link = 'https://www.google.ch/search?num=10&hl=de&site=imghp&tbm=isch&source=hp&bih=824&q="' + properties.Artname + '"'
-        if (properties['Name Deutsch']) {
-          link += '+OR+"' + properties['Name Deutsch'] + '"'
-        }
-        if (properties['Name Französisch']) {
-          link += '+OR+"' + properties['Name Französisch'] + '"'
-        }
-        if (properties['Name Italienisch']) {
-          link += '+OR"' + properties['Name Italienisch'] + '"'
-        }
+        link += `"${Artname}"`
+        if (properties['Name Deutsch']) link += ` OR "${properties['Name Deutsch']}"`
+        if (properties['Name Französisch']) link += ` OR "${properties['Name Französisch']}"`
+        if (properties['Name Italienisch']) link += ` OR "${properties['Name Italienisch']}"`
         break
       case 'Moose':
-        link = 'https://www.google.ch/search?num=10&hl=de&site=imghp&tbm=isch&source=hp&bih=824&q="' + properties.Gattung + ' ' + properties.Art + '"'
+        link += `"${Gattung} ${Art}"`
         break
       case 'Macromycetes':
-        link = 'https://www.google.ch/search?num=10&hl=de&site=imghp&tbm=isch&source=hp&bih=824&q="' + properties.Name + '"'
-        if (properties['Name Deutsch']) {
-          link += '+OR+"' + properties['Name Deutsch'] + '"'
-        }
+        link += `"${Name}"`
+        if (properties['Name Deutsch']) link += ` OR "${properties['Name Deutsch']}"`
         break
       case 'Lebensräume':
-        link = 'https://www.google.ch/search?num=10&hl=de&site=imghp&tbm=isch&source=hp&bih=824&q="' + properties.Einheit
+        link += `"${Einheit}"`
         break
     }
     return link
-  } else {
-    return '#'
   }
+  return '#'
 }
