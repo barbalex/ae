36f9a45685b0393b3a655eee8c5f04e4d79abd4f
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-11 17:09:46 +0200

    optimize code

--- src/components/main/importPc/getObjectsFromFile.js
+++ src/components/main/importPc/getObjectsFromFile.js
@@ -12,14 +12,14 @@ export default (file) => {
     if (fileType === 'csv') {
       reader.onload = (onloadEvent) => {
         const data = onloadEvent.target.result
-        const pcsToImport = d3.csv.parse(data)
+        const objects = d3.csv.parse(data)
         // d3 adds missing fields as '' > remove them
-        pcsToImport.forEach((pc, index) => {
+        objects.forEach((pc, index) => {
           _.forEach(pc, (value, key) => {
-            if (value === '') delete pcsToImport[index][key]
+            if (value === '') delete objects[index][key]
           })
         })
-        resolve(pcsToImport)
+        resolve(objects)
       }
       reader.onerror = (error) => reject('error reading file: ' + error)
       reader.readAsText(file)
@@ -30,8 +30,8 @@ export default (file) => {
         const workbook = window.XLSX.read(data, {type: 'binary'})
         const sheetName = workbook.SheetNames[0]
         const worksheet = workbook.Sheets[sheetName]
-        const pcsToImport = window.XLSX.utils.sheet_to_json(worksheet)
-        resolve(pcsToImport)
+        const objects = window.XLSX.utils.sheet_to_json(worksheet)
+        resolve(objects)
       }
       reader.onerror = (error) => reject('error reading file: ' + error)
       reader.readAsBinaryString(file)
