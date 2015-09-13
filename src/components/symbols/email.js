4a646bf96c9f46c6aadbb64a652c5bdfc4dcc1e1
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-13 18:54:11 +0200

    use dropdown for email menu

--- src/components/symbols/email.js
+++ src/components/symbols/email.js
@@ -2,56 +2,86 @@
 
 import app from 'ampersand-app'
 import React from 'react'
-import { OverlayTrigger, Popover } from 'react-bootstrap'
+import { OverlayTrigger, Popover, Dropdown, MenuItem } from 'react-bootstrap'
 
 export default React.createClass({
   displayName: 'Email',
 
   propTypes: {
-    email: React.PropTypes.string
+    email: React.PropTypes.string,
+    open: React.PropTypes.bool
   },
 
-  onClickAbmelden () {
-    console.log('abmelden clicked')
+  getInitialState () {
+    return {
+      open: false
+    }
+  },
+
+  toggleDropdown () {
+    let { open } = this.state
+    open = !open
+    this.setState({ open })
+    // this is needed to close the menu if user clicks outside of the dropdown
+    if (open) document.addEventListener('click', this.onClickDocument)
+  },
+
+  abmelden () {
     app.Actions.login({
       logIn: false,
       email: null
     })
+    this.setState({ open: false })
+    document.removeEventListener('click', this.onClickDocument)
   },
 
-  onClickAnmelden () {
+  anmelden () {
     const loginVariables = {
       logIn: true,
       email: undefined
     }
     app.Actions.login(loginVariables)
+    this.setState({ open: false })
+    document.removeEventListener('click', this.onClickDocument)
+  },
+
+  /**
+   * this is needed to close the menu if user clicks outside of the dropdown
+   */
+  onClickDocument () {
+    let { open } = this.state
+    if (open) {
+      open = !open
+      this.setState({ open })
+      document.removeEventListener('click', this.onClickDocument)
+    }
   },
 
   render () {
     const { email } = this.props
-    const popoverStyle = {
-      cursor: 'pointer'
-    }
+    const { open } = this.state
 
     if (email) {
       return (
-        <OverlayTrigger
-          trigger='click'
-          placement='left'
-          rootClose={true}
-          overlay={<Popover id='emailPopover' arrowOffsetTop='10' onClick={this.onClickAbmelden}><p style={popoverStyle}>abmelden</p></Popover>}>
-          <div id='email' className='symbols link'>{email}</div>
-        </OverlayTrigger>
+        <Dropdown id='emailDropdown' open={open}>
+          <p bsRole='toggle' className='symbols link' onClick={this.toggleDropdown}>
+            {email}
+          </p>
+          <div bsRole='menu' className='dropdown-menu'>
+            <MenuItem onSelect={this.abmelden}>abmelden</MenuItem>
+          </div>
+        </Dropdown>
       )
     }
     return (
-      <OverlayTrigger
-        trigger='click'
-        placement='left'
-        rootClose={true}
-        overlay={<Popover id='emailPopover' arrowOffsetTop='10' onClick={this.onClickAnmelden}><p style={popoverStyle}>anmelden</p></Popover>}>
-        <div id='email' className='symbols link'>nicht angemeldet</div>
-      </OverlayTrigger>
+      <Dropdown id='emailDropdown' open={open}>
+        <p bsRole='toggle' className='symbols link' onClick={this.toggleDropdown}>
+          nicht angemeldet
+        </p>
+        <div bsRole='menu' className='dropdown-menu'>
+          <MenuItem onSelect={this.anmelden}>anmelden</MenuItem>
+        </div>
+      </Dropdown>
     )
   }
 })
