a5b2cb006768846a2a72b551327ccdc3b16daf77
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-16 21:51:46 +0200

    explain well

--- src/components/main/export/wellTaxonomienZusammenfassen.js
+++ src/components/main/export/wellTaxonomienZusammenfassen.js
@@ -23,15 +23,31 @@ export default React.createClass({
 
   render () {
     const { visible } = this.state
+    const pStyle = {
+      marginLeft: 23,
+      marginBottom: 0
+    }
 
     return (
-      <Well className='well-sm'><b>Taxonomien zusammenfassen?</b> <a href='#' onClick={this.onClickToggle} className='showNextHidden'>{visible ? '...weniger' : '...mehr'}</a>
-        <ul className='adb-hidden' style={{'display': visible ? 'block' : 'none'}}>
-          <li>Mit dieser Option werden alle in den gewählten Gruppen vorkommenden Taxonomien unter dem Titel "Taxonomie(n)" zusammegefasst und darunter alle in diesen Taxonomien vorkommenden Felder aufgelistet</li>
-          <li>Dabei werden die Daten gleich lautender Felder aus allen Taxonomien ins selbe Feld geschrieben</li>
-          <li>Beispielsweise können Sie bei Lebensräumen im Feld "Taxonomie" nach "Delarze" filtern um alle Taxonomien von Delarze gleichzeitig zu exportieren</li>
-          <li>Oder Sie können z.B. Arten aus Flora und Fauna gleichzeitig exportieren und dabei den vollständigen Artnamen ins selbe Feld schreiben</li>
-        </ul>
+      <Well className='well-sm'>
+        <b>Taxonomien zusammenfassen?</b> <a href='#' onClick={this.onClickToggle} className='showNextHidden'>{visible ? '...weniger' : '...mehr'}</a>
+        <div className='adb-hidden' style={{'display': visible ? 'block' : 'none'}}>
+          <ul>
+            <li>Mit dieser Option werden alle in den gewählten Gruppen vorkommenden Taxonomien unter dem Titel "Taxonomie(n)" zusammegefasst und darunter alle in diesen Taxonomien vorkommenden Felder aufgelistet</li>
+            <li>Dabei werden die Daten gleich lautender Felder aus allen Taxonomien ins selbe Feld geschrieben ("zusammegefasst"), und zwar:
+              <ul>
+                <li>schon beim filtern und Eigenschaften wählen</li>
+                <li>...und natürlich beim exportieren</li>
+              </ul>
+            </li>
+            <li>Nicht beeinflusst werden Felder aus Eigenschaften- oder Beziehungssammlungen</li>
+          </ul>
+          <p style={pStyle}>Beispiele:</p>
+          <ul>
+            <li>Sie können bei Lebensräumen im Feld "Taxonomie" nach "Delarze" filtern um alle Taxonomien von Delarze gleichzeitig zu exportieren</li>
+            <li>Sie können Arten aus Flora und Fauna gleichzeitig exportieren und dabei den vollständigen Artnamen ins selbe Feld ("Artname vollständig") schreiben</li>
+          </ul>
+        </div>
       </Well>
     )
   }
