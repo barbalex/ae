82ef244195cf538053b87e08080d597792533d8f
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-17 14:55:29 +0200

    show loading groups symbol when loading groups

---
+++ src/components/symbols/groupsLoading.js
@@ -0,0 +1,29 @@
+'use strict'
+
+import React from 'react'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
+
+export default React.createClass({
+  displayName: 'GroupsLoading',
+
+  propTypes: {
+    groupsLoadingObjects: React.PropTypes.array
+  },
+
+  render () {
+    const { groupsLoadingObjects } = this.props
+    const tooltip = <Tooltip id='groupsLoadingTooltip' bsStyle='info'>Bitte Geduld: Die App kann zeitweise einfrieren!</Tooltip>
+    const text = groupsLoadingObjects.length > 1 ? 'Lade Gruppen...' : 'Lade Gruppe...'
+    const pStyle = {
+      color: 'red',
+      fontWeight: 500,
+      cursor: 'progress'
+    }
+
+    return (
+      <OverlayTrigger placement='left' overlay={tooltip}>
+        <p className='symbols text' style={pStyle}>{text}</p>
+      </OverlayTrigger>
+    )
+  }
+})
--- src/components/symbols/symbols.js
+++ src/components/symbols/symbols.js
@@ -4,15 +4,19 @@ import React from 'react'
 import ReplicatingToAe from './replicatingToAe.js'
 import ReplicatingFromAe from './replicatingFromAe.js'
 import Email from './email.js'
+import GroupsLoading from './groupsLoading.js'
 import PcsQuerying from './pcsQuerying.js'
 import RcsQuerying from './rcsQuerying.js'
+import FieldsQuerying from './fieldsQuerying.js'
 
 export default React.createClass({
   displayName: 'Symbols',
 
   propTypes: {
+    groupsLoadingObjects: React.PropTypes.array,
     pcsQuerying: React.PropTypes.bool,
     rcsQuerying: React.PropTypes.bool,
+    fieldsQuerying: React.PropTypes.bool,
     email: React.PropTypes.string,
     replicatingToAe: React.PropTypes.string,
     replicatingToAeTime: React.PropTypes.string,
@@ -21,9 +25,10 @@ export default React.createClass({
   },
 
   render () {
-    const { email, replicatingToAe, replicatingToAeTime, replicatingFromAe, replicatingFromAeTime, pcsQuerying, rcsQuerying } = this.props
+    const { email, replicatingToAe, replicatingToAeTime, replicatingFromAe, replicatingFromAeTime, groupsLoadingObjects, pcsQuerying, rcsQuerying, fieldsQuerying } = this.props
     const showReplicatingToAe = replicatingToAe !== null
     const showReplicatingFromAe = replicatingFromAe !== null
+    const showGroupsLoading = groupsLoadingObjects.length > 0
 
     return (
       <div id='symbols'>
@@ -42,6 +47,16 @@ export default React.createClass({
           </div>
           <div className='symbol-div'>
             {rcsQuerying ? <RcsQuerying /> : null}
+          </div>
+          <div className='symbol-div'>
+            {fieldsQuerying ? <FieldsQuerying /> : null}
+          </div>
+          <div className='symbol-div'>
+            {showGroupsLoading ?
+              <GroupsLoading
+                groupsLoadingObjects={groupsLoadingObjects} />
+              : null
+            }
           </div>
         </div>
       </div>
