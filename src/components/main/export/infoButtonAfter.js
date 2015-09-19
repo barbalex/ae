da2f283311a52e8e43190a135ece04a09ac242d4
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-20 01:01:58 +0200

    show groups nicer

--- src/components/main/export/infoButtonAfter.js
+++ src/components/main/export/infoButtonAfter.js
@@ -13,7 +13,7 @@ export default React.createClass({
   render () {
     const { fNameObject } = this.props
     const id = `${fNameObject.cName}-${fNameObject.fName}-info`
-    const tooltip = <Tooltip id={id}>Gruppen: {fNameObject.groups}<br/>Anzahl Objekte: {fNameObject.count}</Tooltip>
+    const tooltip = <Tooltip id={id}>Gruppen: {fNameObject.groups.join(', ')}<br/>Anzahl Objekte: {fNameObject.count}</Tooltip>
     return (
       <OverlayTrigger placement='left' trigger={['click']} rootClose overlay={tooltip}>
         <Button bsSize='small' style={{marginTop: -5}}><Glyphicon glyph='info-sign' style={{fontSize: 14}} /></Button>
