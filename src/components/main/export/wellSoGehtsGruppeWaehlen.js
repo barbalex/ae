9c71273d00c80eadb9964a3aff1e3e99c655c9de
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-20 21:40:18 +0200

    explain better

--- src/components/main/export/wellSoGehtsGruppeWaehlen.js
+++ src/components/main/export/wellSoGehtsGruppeWaehlen.js
@@ -28,7 +28,7 @@ export default React.createClass({
       <Well className='well-sm'><b>So geht`s</b> <a href='#' onClick={this.onClickToggle} className='showNextHidden'>{visible ? '...weniger' : '...mehr'}</a>
         <ul className='adb-hidden' style={{'display': visible ? 'block' : 'none'}}>
           <li>Wählen Sie eine oder mehrere Gruppen...</li>
-          <li>...dann werden die Eigenschaften der Gruppe(n) aufgebaut...</li>
+          <li>...dann werden ihre Eigenschaften aufgebaut...</li>
           <li>...und Sie können filtern und Eigenschaften wählen</li>
           <li><a href='//youtu.be/J13wS88pYC8' target='_blank'>Screencast sehen</a></li>
         </ul>
