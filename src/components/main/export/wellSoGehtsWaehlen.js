69d8b737b690faa00916558517ba47c1b4b46f26
Alexander Gabriel <alex@gabriel-software.ch>
2015-09-22 16:38:55 +0200

    add well

---
+++ src/components/main/export/wellSoGehtsWaehlen.js
@@ -0,0 +1,41 @@
+'use strict'
+
+import React from 'react'
+import { Well } from 'react-bootstrap'
+
+export default React.createClass({
+  displayName: 'WellSoGehtsWählen',
+
+  propTypes: {
+    visible: React.PropTypes.bool
+  },
+
+  getInitialState () {
+    return {
+      visible: false
+    }
+  },
+
+  onClickToggle (event) {
+    event.preventDefault()
+    this.setState({ visible: !this.state.visible })
+  },
+
+  render () {
+    const { visible } = this.state
+    const wellStyle = {
+      marginBottom: 5 + 'px !important'
+    }
+
+    return (
+      <Well className='well-sm' style={wellStyle}>
+        <b>So geht`s</b> <a href='#' onClick={this.onClickToggle} className='showNextHidden'>{visible ? '...weniger' : '...mehr'}</a>
+        <ul className='adb-hidden' style={{'display': visible ? 'block' : 'none'}}>
+          <li>Nachfolgend sind alle Eigenschaften aufgelistet, die in den gewählten Gruppen vorkommen</li>
+          <li>Markieren Sie die Eigenschaften, die Sie exportieren möchten...</li>
+          <li>...und fahren Sie danach mit "4. exportieren" weiter</li>
+        </ul>
+      </Well>
+    )
+  }
+})
