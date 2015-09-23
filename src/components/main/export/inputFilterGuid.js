0b2a5e1cefc4736bc2833af0267ed314fb8e827f
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-23 10:20:55 +0200

    validate input and alert result

--- src/components/main/export/inputFilterGuid.js
+++ src/components/main/export/inputFilterGuid.js
@@ -1,18 +1,59 @@
 'use strict'
 
 import React from 'react'
-import { OverlayTrigger, Popover } from 'react-bootstrap'
+import { OverlayTrigger, Popover, Alert } from 'react-bootstrap'
+import isGuid from '../../../modules/isGuid.js'
 
 export default React.createClass({
   displayName: 'InputFilterGuid',
 
   propTypes: {
-    onChangeFilterField: React.PropTypes.func
+    onChangeFilterField: React.PropTypes.func,
+    invalidGuids: React.PropTypes.array
+  },
+
+  getInitialState () {
+    return {
+      invalidGuids: []
+    }
   },
 
   onBlur (cName, fName, event) {
     const { onChangeFilterField } = this.props
-    onChangeFilterField(cName, fName, event)
+    let invalidGuids = []
+    let value = event.target.value
+    // one or more guids were entered
+    if (value) {
+      // remove empty strings from value
+      value = value.replace(/\s+/g, '')
+      if (value) {
+        // convert value into array
+        // this way user can enter a single guid or many comma separated
+        value = value.split(',')
+        // check if values are valid guids
+        value.forEach((val, index) => {
+          if (val === null || val === '') {
+            value.splice(index, 1)
+          } else if (!isGuid(val)) {
+            invalidGuids.push(val)
+          }
+        })
+      } else {
+        value = null
+      }
+    } else {
+      value = null
+    }
+    this.setState({ invalidGuids })
+    // build a event object that passes the array as an array - event.target.value passes it as string
+    const eventToPass = {
+      target: {
+        value: value
+      }
+    }
+    console.log('InputFilterGuid, value', value)
+    console.log('InputFilterGuid, event.target.value', event.target.value)
+    if (invalidGuids.length === 0) onChangeFilterField(cName, fName, eventToPass)
   },
 
   popover () {
@@ -34,17 +75,39 @@ export default React.createClass({
     )
   },
 
+  invalidGuids () {
+    const { invalidGuids } = this.state
+    const alertStyle = {
+      marginTop: 8
+    }
+    const list = invalidGuids.map((guid, index) => <li key={index}>{guid}</li>)
+    return (
+      <Alert bsStyle='danger' className='controls feld' style={alertStyle}>
+        <p>Bitte prüfen Sie die folgenden GUID`s:</p>
+        <ul>
+          {list}
+        </ul>
+      </Alert>
+    )
+  },
+
   render () {
+    const { invalidGuids } = this.state
+    const showInvalidGuids = invalidGuids.length > 0
     // necessary to align guid with other fields
     const divStyle = {
       marginLeft: 0
     }
+    const formGroupClass = invalidGuids.length > 0 ? 'form-group has-error' : 'form-group'
     return (
-      <div className='form-group' style={divStyle}>
-        <OverlayTrigger trigger={['click', 'focus']} rootClose placement='right' overlay={this.popover()}>
-          <label className='control-label withPopover'>GUID</label>
-        </OverlayTrigger>
-        <textarea className='controls input-sm form-control' onBlur={this.onBlur.bind(this, 'object', '_id')} />
+      <div>
+        <div className={formGroupClass} style={divStyle}>
+          <OverlayTrigger trigger={['click', 'focus']} rootClose placement='right' overlay={this.popover()}>
+            <label className='control-label withPopover'>GUID</label>
+          </OverlayTrigger>
+          <textarea className='controls input-sm form-control' onBlur={this.onBlur.bind(this, 'object', 'ids')} spellCheck={false} />
+          {showInvalidGuids ? this.invalidGuids() : null}
+        </div>
       </div>
     )
   }
