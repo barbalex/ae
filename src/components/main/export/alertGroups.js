785d96c514e923fdb3679317a64c532fd16cbe36
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-24 13:25:30 +0200

    dont show success before all loading is over

--- src/components/main/export/alertGroups.js
+++ src/components/main/export/alertGroups.js
@@ -10,19 +10,22 @@ export default React.createClass({
     taxonomienZusammenfassen: React.PropTypes.bool,
     fieldsQuerying: React.PropTypes.bool,
     fieldsQueryingError: React.PropTypes.object,
-    taxonomyFields: React.PropTypes.object
+    taxonomyFields: React.PropTypes.object,
+    pcsQuerying: React.PropTypes.bool,
+    rcsQuerying: React.PropTypes.bool
   },
 
   render () {
-    const { taxonomienZusammenfassen, fieldsQuerying, fieldsQueryingError, taxonomyFields } = this.props
+    const { taxonomienZusammenfassen, fieldsQuerying, fieldsQueryingError, taxonomyFields, pcsQuerying, rcsQuerying } = this.props
     // fieldsQuerying && taxonomyFields.length === 0
     let resultText = 'Die Eigenschaften werden aufgebaut...'
     let taxonomienZusammenfassenText = null
     let bsStyle = 'info'
-    if (fieldsQuerying && Object.keys(taxonomyFields).length > 0) {
+    const somethingQuerying = fieldsQuerying || pcsQuerying || rcsQuerying
+    if (somethingQuerying && Object.keys(taxonomyFields).length > 0) {
       resultText = 'Die Eigenschaften werden ergänzt...'
     }
-    if (!fieldsQuerying && Object.keys(taxonomyFields).length > 0) {
+    if (!somethingQuerying && Object.keys(taxonomyFields).length > 0) {
       bsStyle = 'success'
       resultText = 'Die Eigenschaften wurden aufgebaut.'
       taxonomienZusammenfassenText = taxonomienZusammenfassen ? 'Taxonomien werden zusammengefasst.' : 'Taxonomien werden einzeln dargestellt.'
