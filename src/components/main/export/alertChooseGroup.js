d31bc2a5dad9bca45697216b1bd17ed96ae4a7be
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-24 15:37:09 +0200

    add alertChooseGroup

---
+++ src/components/main/export/alertChooseGroup.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import React from 'react'
+import { Alert } from 'react-bootstrap'
+
+export default React.createClass({
+  displayName: 'AlertChooseGroup',
+
+  render () {
+    const style = {
+      marginTop: 8,
+      marginBottom: 0
+    }
+    return (
+      <Alert bsStyle='info' style={style}>
+        <p>Bitte wählen Sie mindestens eine Gruppe</p>
+      </Alert>
+    )
+  }
+})
