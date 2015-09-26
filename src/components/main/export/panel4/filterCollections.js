d4a67d42fafe1ddbd0e0de38c4c2c84dede53bb8
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-27 01:45:11 +0200

    make filtering with taxonomienZusammenfassen work correctly

--- src/components/main/export/panel4/filterCollections.js
+++ src/components/main/export/panel4/filterCollections.js
@@ -11,60 +11,39 @@ import _ from 'lodash'
 import isFilterFulfilled from './isFilterFulfilled.js'
 
 export default (exportOptions, objects, taxonomienZusammenfassen) => {
+  console.log('taxonomienZusammenfassen', taxonomienZusammenfassen)
   Object.keys(exportOptions).forEach((cName) => {
     const cType = exportOptions[cName].cType
     if (cType) {
       Object.keys(exportOptions[cName]).forEach((fName) => {
-        if (cName !== 'object') {
+        /**
+         * always exclude cName === 'object'
+         * always exclude fName === 'cType'
+         * if taxonomienZusammenfassen, exclude cType === 'taxonomy'
+         */
+        if (cName !== 'object' && fName !== 'cType' && !(cType === 'taxonomy' && taxonomienZusammenfassen)) {
           const filterValue = exportOptions[cName][fName].value
           const co = exportOptions[cName][fName].co
           objects = _.filter(objects, (object) => {
             // find collection with this name
-            let collections = object.Taxonomien
+            let collections = []
+            if (cType === 'taxonomy' && !taxonomienZusammenfassen) collections = object.Taxonomien
             if (cType === 'pc') collections = object.Eigenschaftensammlungen
             if (cType === 'rc') collections = object.Beziehungssammlungen
-            const collection = _.find(collections, (co) => co.Name === cName)
+            const collection = _.find(collections, (c) => c.Name === cName)
             if (collection) {
               // if taxonomy or pc, check directly
               if (cType !== 'rc') return isFilterFulfilled(collection.Eigenschaften[fName], filterValue, co)
               // if rc, check if any relation fulfills
               const relations = collection.Beziehungen
               if (relations && relations.length > 0) {
+                let returnFromRelationsLoop = false
                 relations.forEach((relation) => {
-                  if (isFilterFulfilled(relation[fName], filterValue, co)) return true
+                  if (isFilterFulfilled(relation[fName], filterValue, co)) {
+                    returnFromRelationsLoop = true
+                  }
                 })
-                return false
-              }
-              return false
-            }
-            return false
-          })
-        }
-      })
-    }
-    if (taxonomienZusammenfassen) {
-      /**
-       * exportOptions does not contain cNames of the taxonomies
-       * instead it contains 'Taxonomie(n)'
-       */
-      Object.keys(exportOptions[cName]).forEach((fName) => {
-        if (cType === 'taxonomy') {
-          const filterValue = exportOptions['Taxonomie(n)'][fName].value
-          const co = exportOptions['Taxonomie(n)'][fName].co
-          objects = _.filter(objects, (object) => {
-            // find collection with this name
-            let collections = object.Taxonomien
-            const collection = _.find(collections, (co) => co.Name === cName)
-            if (collection) {
-              // if taxonomy or pc, check directly
-              if (cType !== 'rc') return isFilterFulfilled(collection.Eigenschaften[fName], filterValue, co)
-              // if rc, check if any relation fulfills
-              const relations = collection.Beziehungen
-              if (relations && relations.length > 0) {
-                relations.forEach((relation) => {
-                  if (isFilterFulfilled(relation[fName], filterValue, co)) return true
-                })
-                return false
+                return returnFromRelationsLoop
               }
               return false
             }
@@ -74,5 +53,26 @@ export default (exportOptions, objects, taxonomienZusammenfassen) => {
       })
     }
   })
+  if (taxonomienZusammenfassen) {
+    /**
+     * exportOptions does not contain cNames of the taxonomies
+     * instead it contains 'Taxonomie(n)'
+     */
+    Object.keys(exportOptions['Taxonomie(n)']).forEach((fName) => {
+      if (fName !== 'cType') {
+        const filterValue = exportOptions['Taxonomie(n)'][fName].value
+        const co = exportOptions['Taxonomie(n)'][fName].co
+        objects = _.filter(objects, (object) => {
+          let returnFromRelationsLoop = false
+          object.Taxonomien.forEach((taxonomy) => {
+            if (isFilterFulfilled(taxonomy.Eigenschaften[fName], filterValue, co)) {
+              returnFromRelationsLoop = true
+            }
+          })
+          return returnFromRelationsLoop
+        })
+      }
+    })
+  }
   return objects
 }
