a4eb8c00491022f74ed791e10bd6ff88c7664b52
Alexander Gabriel <alex@gabriel-software.ch>
2015-09-28 16:43:04 +0200

    get download as csv to work

--- src/components/main/export/panel4/download.js
+++ src/components/main/export/panel4/download.js
@@ -1,18 +1,21 @@
 'use strict'
 
-// import d3 from 'd3'
+import d3 from 'd3'
 import moment from 'moment'
 import createBlobXlsx from './createBlobXlsx.js'
 import FileSaver from 'browser-filesaver'
 
 export default (exportObjects, format) => {
   const date = moment().format('YYYY-MM-DD_HH-mm')
+  const filename = `${date}_arteigenschaften.${format}`
+  let blob
   if (format === 'csv') {
-
+    const csvData = d3.csv.format(exportObjects)
+    blob = new window.Blob([csvData], {type: 'text/plain;charset=utf-8;'})
   }
   if (format === 'xlsx') {
     const blobData = createBlobXlsx(exportObjects)
-    const blob = new window.Blob([blobData], {type: 'application/octet-stream;charset=utf-8;'})
-    FileSaver.saveAs(blob, `${date}_arteigenschaften.xlsx`)
+    blob = new window.Blob([blobData], {type: 'application/octet-stream;charset=utf-8;'})
   }
+  FileSaver.saveAs(blob, filename)
 }
