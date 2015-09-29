dedc888e5b4d75d03509b8ac5aacbb7849247a03
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-09-29 23:37:49 +0200

    build on for rc's

--- src/components/main/export/panel4/buildExportObjects.js
+++ src/components/main/export/panel4/buildExportObjects.js
@@ -9,41 +9,22 @@
 
 import _ from 'lodash'
 
-function getValueOfField (object, cType, cName, fName) {
-  let cTypeName
-  switch (cType) {
-  case 'taxonomy':
-    cTypeName = 'Taxonomien'
-    break
-  case 'pc':
-    cTypeName = 'Eigenschaftensammlungen'
-    break
-  case 'rc':
-    cTypeName = 'Beziehungssammlungen'
-    break
-  default:
-    return null
-  }
-  const collection = _.find(object[cTypeName], (c) => c.Name === cName)
-  if (collection) return collection.Eigenschaften[fName]
-  return null
-}
-
-export default (exportOptions, objects, combineTaxonomies) => {
+export default (exportOptions, objects, combineTaxonomies, oneRowPerRelation) => {
   let exportObjects = []
+  let fieldsToAddToAllExportObjects = []
   objects.forEach((object) => {
     let exportObject = {}
-    // add _id if applicable
+    // 1. add _id if applicable
     if (_.get(exportOptions, 'object._id.export')) {
       const value = _.get(object, '_id', null)
       exportObject.GUID = value
     }
-    // add Gruppen if applicable
+    // 2. add Gruppen if applicable
     if (_.get(exportOptions, 'object.Gruppen.export')) {
       const value = _.get(object, 'Gruppe', null)
       exportObject.Gruppe = value
     }
-    // push any other pc or rc field
+    // 3. push any other pc or rc field
     Object.keys(exportOptions).forEach((cName) => {
       const cType = exportOptions[cName].cType
       if (cType) {
@@ -54,15 +35,83 @@ export default (exportOptions, objects, combineTaxonomies) => {
             /**
              * get value of this field in object
              */
-            const value = getValueOfField(object, cType, cName, fName)
-            const key = `${cName}: ${fName}`
-            exportObject[key] = value
+            let cTypeName
+            switch (cType) {
+            case 'taxonomy':
+              cTypeName = 'Taxonomien'
+              break
+            case 'pc':
+              cTypeName = 'Eigenschaftensammlungen'
+              break
+            case 'rc':
+              cTypeName = 'Beziehungssammlungen'
+              break
+            default:
+              return null
+            }
+            const collection = _.find(object[cTypeName], (c) => c.Name === cName)
+            if (collection) {
+              if (cType !== 'rc') {
+                const value = collection.Eigenschaften[fName]
+                const key = `${cName}: ${fName}`
+                exportObject[key] = value
+              } else {
+                // now handle rc
+                const relations = collection.Beziehungen
+                if (relations && relations.length > 0) {
+                  /**
+                   * next step depends on oneRowPerRelation
+                   * if true: clone exportObject for every relation and:
+                   *   - create field cName: Beziehungspartner
+                   *     contains JSON.stringify of Beziehungspartner
+                   *   - create field cName: Beziehungspartner GUID
+                   *     contains the GUID
+                   *   - create more fields if they exist
+                   *   - in the end: add thes field names to all exportObjects
+                   * if false:
+                   *   - create field cName: Beziehungspartner
+                   *     contains for every relation JSON.stringify(Beziehungspartner)
+                   *   - create field cName: Beziehungspartner GUID's
+                   *     contains an array of the rPartners GUID's
+                   *   - if more fields: create them
+                   *     contain an array of the values of the field
+                   *   - in the end: add thes field names to all exportObjects
+                   */
+                  relations.forEach((relation, rIndex) => {
+                    if (fName !== 'Beziehungspartner') {
+                      const value = relation[fName]
+                      const key = `${cName}: ${fName}`
+                      exportObject[key] = value
+                    } else {
+                      // this is Beziehungspartner
+                      // we want to return an array of rPartner objects and an array of GUIDs
+                      const rPartners = relations[rIndex][fName]
+                      const rPartnerObjects = rPartners.map((rPartner) => JSON.stringify(rPartner))
+                      const rPartnerGuids = _.pluck(rPartners, 'GUID')
+                    }
+                  })
+                }
+              }
+            }
           }
         })
       }
     })
-    exportObjects.push(exportObject)
+    /**
+     * exportObject can either be a single object
+     * or an array of objects (relations, oneRowPerRelation = false)
+     */
+    if (_.isArray(exportObject)) {
+      exportObjects = exportObjects.concat(exportObject)
+    } else {
+      exportObjects.push(exportObject)
+    }
   })
   // console.log('buildExportObjects.js: exportObjects', exportObjects)
+  if (fieldsToAddToAllExportObjects.length > 0) {
+    // TODO: add all these fields to every export object if it does not already exist
+    // BUT: how get right order?
+    // like this: rebuild every export object new
+  }
   return exportObjects
 }
