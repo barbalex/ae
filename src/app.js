7924f75898f76fcff0d6768ab723798d72e7ed7f
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-10-01 11:52:18 +0200

    seed db's in parallel

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 'use strict'
 
-// import needed dependencies (more will be imported by dependant modules)
+/**
+ * import needed dependencies (more will be imported by dependant modules)
+ */
 import app from 'ampersand-app'
 import PouchDB from 'pouchdb'
 import pouchdbUpsert from 'pouchdb-upsert'
@@ -12,30 +14,44 @@ import pouchUrl from './modules/getCouchUrl.js'
 import getGroupsLoadedFromLocalGroupsDb from './modules/getGroupsLoadedFromLocalGroupsDb.js'
 // make webpack import styles
 import './styles/main.styl'
-// this lib can not be imported directly - need to load it as a global using https://github.com/webpack/script-loader
-import 'script!../node_modules/xlsx/dist/xlsx.core.min.js'
 // make webpack import server.js
 import 'file?name=server.js!../server.js'
+// this lib can not be imported directly - need to load it as a global using https://github.com/webpack/script-loader
+import 'script!../node_modules/xlsx/dist/xlsx.core.min.js'
 
-// set up pouchdb plugins
+/**
+ * set up pouchdb plugins
+ */
 PouchDB.plugin(pouchdbUpsert)
 PouchDB.plugin(pouchdbAuthentication)
 
-// expose 'app' to the browser console
-// this is handy to call actions and stores in the browser console
+/**
+ * expose 'app' to the browser console
+ * this is handy to call actions and stores in the browser console
+ */
 window.app = app
-// enable pouch inspector (https://chrome.google.com/webstore/detail/pouchdb-inspector/hbhhpaojmpfimakffndmpmpndcmonkfa)
+/**
+ * enable pouch inspector in chrome
+ * (https://chrome.google.com/webstore/detail/pouchdb-inspector/hbhhpaojmpfimakffndmpmpndcmonkfa)
+ */
 window.PouchDB = PouchDB
 
-// ampersand-app is extended with app methods (=singleton)
-// modules that need an app method import ampersand-app instead of using a global
+/**
+ * ampersand-app is extended with app methods (=singleton)
+ * modules that need an app method import ampersand-app instead of using a global
+ */
 app.extend({
   init () {
-    // pouchdb keeps setting a lot of listeners which makes browsers show warnings in the console
-    // up the number of listeners to reduce the number of console warnings
+    /**
+     * pouchdb keeps setting a lot of listeners which makes browsers show warnings in the console
+     * up the number of listeners to reduce the number of console warnings
+     */
     PouchDB.setMaxListeners(80)
-    // set up all the needed databases in parallel
-    // in chrome these can be looked at using pouch inspector (https://chrome.google.com/webstore/detail/pouchdb-inspector/hbhhpaojmpfimakffndmpmpndcmonkfa)
+    /**
+     * set up all the needed databases in parallel
+     * in chrome these can be looked at using pouch inspector
+     * (https://chrome.google.com/webstore/detail/pouchdb-inspector/hbhhpaojmpfimakffndmpmpndcmonkfa)
+     */
     Promise.all([
       this.localDb = new PouchDB('ae'),
       this.localHierarchyDb = new PouchDB('aeHierarchy'),
@@ -44,41 +60,50 @@ app.extend({
       this.localFilterOptionsDb = new PouchDB('aeFilterOptions'),
       this.remoteDb = new PouchDB(pouchUrl())
     ])
-    // get meaningful messages when errors occur in design docs
-    // this.localDb.on('error', function (err) { debugger })
-    // initiate localGroupsDb if necessary
-    // putIfNotExists is a method added by pouchdbUpsert
-    .then(() => this.localGroupsDb.putIfNotExists({
-      _id: 'groups',
-      groupsLoaded: []
-    }))
-    // initiate login data if necessary
-    // by adding a local document to pouch
-    // local documents are not replicated
-    .then(() => this.localDb.putIfNotExists({
-      _id: '_local/login',
-      logIn: false,
-      email: null
-    }))
-    // initiate pcs data if necessary
-    .then(() => this.localDb.putIfNotExists({
-      _id: '_local/pcs',
-      pcs: []
-    }))
-    // initiate rcs data if necessary
-    .then(() => this.localDb.putIfNotExists({
-      _id: '_local/rcs',
-      rcs: []
-    }))
-    // initiate fields data if necessary
-    .then(() => this.localDb.putIfNotExists({
-      _id: '_local/fields',
-      fields: []
-    }))
+    .then(() => Promise.all([
+      /**
+       * initiate localGroupsDb if necessary
+       * putIfNotExists is a method added by pouchdbUpsert
+       */
+      this.localGroupsDb.putIfNotExists({
+        _id: 'groups',
+        groupsLoaded: []
+      }),
+      /**
+       * initiate login data if necessary
+       * by adding a local document to pouch
+       * local documents are not replicated
+       */
+      this.localDb.putIfNotExists({
+        _id: '_local/login',
+        logIn: false,
+        email: null
+      }),
+      // initiate pcs data if necessary
+      this.localDb.putIfNotExists({
+        _id: '_local/pcs',
+        pcs: []
+      }),
+      // initiate rcs data if necessary
+      this.localDb.putIfNotExists({
+        _id: '_local/rcs',
+        rcs: []
+      }),
+      // initiate fields data if necessary
+      this.localDb.putIfNotExists({
+        _id: '_local/fields',
+        fields: []
+      })
+    ]))
     .then(() => {
-      // initiate actions, stores and router
-      // extend app with them so they can be called in modules
-      // and accessed in the browser console
+      // get meaningful messages when errors occur in design docs
+      // this.localDb.on('error', function (err) { debugger })
+
+      /**
+       * initiate actions, stores and router
+       * extend app with them so they can be called in modules
+       * and accessed in the browser console
+       */
       this.Actions = actions()
       stores(this.Actions)
       this.router = new Router()
