e2604199bc7d8a6bf9ceed949b3fbd56a3d8a26d
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-11-19 17:29:31 +0100

    optimize config

--- webpack.config.js
+++ webpack.config.js
@@ -27,14 +27,6 @@ config.module.loaders.push(
     query: {
       presets: ['es2015', 'react', 'stage-2']
     }
-  },
-  {
-    test: /\.js?$/,
-    exclude: /(node_modules|bower_components)/,
-    loader: 'babel',
-    query: {
-      presets: ['es2015', 'react', 'stage-2']
-    }
   }
 )
 
