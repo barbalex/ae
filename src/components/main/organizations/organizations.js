b25dac9184cd2aff136127a78502023705ef7829
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-12-04 12:13:47 +0100

    structure organizations in own folder

---
+++ src/components/main/organizations/organizations.js
@@ -0,0 +1,40 @@
+'use strict'
+
+import app from 'ampersand-app'
+import React from 'react'
+import { Accordion, Panel } from 'react-bootstrap'
+
+export default React.createClass({
+  displayName: 'Organizations',
+
+  propTypes: {
+    email: React.PropTypes.string
+  },
+
+  componentDidMount () {
+    const { email } = this.props
+    if (!email) {
+      const loginVariables = {
+        logIn: true,
+        email: undefined
+      }
+      app.Actions.login(loginVariables)
+    }
+  },
+
+  render () {
+    return (
+      <div className='formContent'>
+        <h4>Organisationen und Benutzer</h4>
+        <Accordion>
+          <Panel header='Organisation' eventKey='1'>
+            testdata
+          </Panel>
+          <Panel header='Benutzer' eventKey='2'>
+            testdata
+          </Panel>
+        </Accordion>
+      </div>
+    )
+  }
+})
