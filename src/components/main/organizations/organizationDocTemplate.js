4e4359c17658607116d0fd6bc88971d00874138a
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-12-04 14:44:22 +0100

    add org doc template

---
+++ src/components/main/organizations/organizationDocTemplate.js
@@ -0,0 +1,9 @@
+{
+  "Typ": "Organisation",
+  "Name": "FNS Kt. ZH",
+  "esSchreiber": ["alex@gabriel-software.ch"],
+  "lrSchreiber": ["alex@gabriel-software.ch"],
+  "orgAdministratoren": ["andreas.lienhard@bd.zh.ch"]
+}
+
+619673f20b3af1ce4ce73111a60005b4
