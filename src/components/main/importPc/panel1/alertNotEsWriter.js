0e95cb4f337c70ae670fc0ee0ddfb95e64a106d1
Alexander Gabriel <alexander.gabriel@bluewin.ch>
2015-12-08 18:53:26 +0100

    begin with alert not writer

---
+++ src/components/main/importPc/panel1/alertNotEsWriter.js
@@ -0,0 +1,27 @@
+'use strict'
+
+import React from 'react'
+import { Alert } from 'react-bootstrap'
+
+export default React.createClass({
+  displayName: 'AlertNotEsWriter',
+
+  render () {
+    const style = {
+      marginBottom: 5
+    }
+    return (
+      <div
+        className='form-group'>
+        <Alert
+          className='feld'
+          bsStyle='danger'
+          style={style}>
+          Importieren können Sie nur, wenn Ihnen von einer Organisation entsprechende Schreibrechte erteilt wurden.<br/>
+          Da Sie keine Schreibrechte haben, können Sie keine Eigenschaften importieren.<br/>
+          Um Importieren zu können, wenden Sie sich bitte an eine der beteiligten Organisationen, z.B. <a href='mailto:andreas.lienhard@bd.zh.ch'>die Fachstelle Naturschutz des Kantons Zürich</a>.
+        </Alert>
+      </div>
+    )
+  }
+})
