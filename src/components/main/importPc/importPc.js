b1b89e408b3505880f01b3941190211f608b06fd
Alexander Gabriel <alex.barbalex@gmail.com>
2015-12-08 10:48:16 +0100

    work on esWriters

--- src/components/main/importPc/importPc.js
+++ src/components/main/importPc/importPc.js
@@ -74,6 +74,8 @@ export default React.createClass({
   // panel1Done, panel2Done, panel3Done: to guide inputting
   // validXxx: to check validity of these fields
   getInitialState () {
+    const { userIsEsWriterInOrgs } = this.props
+    const orgMitSchreibrecht = userIsEsWriterInOrgs.length === 1 ? userIsEsWriterInOrgs[0] : null
     return {
       nameBestehend: null,
       name: null,
@@ -81,7 +83,7 @@ export default React.createClass({
       datenstand: null,
       nutzungsbedingungen: null,
       link: null,
-      orgMitSchreibrecht: null,
+      orgMitSchreibrecht: orgMitSchreibrecht,
       importiertVon: this.props.email,
       zusammenfassend: null,
       nameUrsprungsEs: null,
