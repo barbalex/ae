28100acd01843a16a864952bd3aae9be7d5a9b4f
Alexander Gabriel <alex.barbalex@gmail.com>
2015-12-09 11:39:27 +0100

    optimize code

--- src/components/main/login.js
+++ src/components/main/login.js
@@ -123,30 +123,96 @@ export default React.createClass({
 
     return (
       <div className='static-modal'>
-        <Modal.Dialog onHide={this.onHide}>
+        <Modal.Dialog
+          onHide={this.onHide}>
           <Modal.Header>
-            <Modal.Title>Anmelden</Modal.Title>
+            <Modal.Title>
+              Anmelden
+            </Modal.Title>
           </Modal.Header>
 
           <Modal.Body>
-            <form className={'form'} autoComplete='off'>
-              <p className='anmelden'>Für diese Funktion müssen Sie angemeldet sein.<br/><a href='mailto:alex@gabriel-software.ch'>Mailen Sie mir</a>, um ein Login zu erhalten.</p>
-              <div className='formGroup'>
-                <Input type='email' id='email' label={'Email'} bsSize='small' className={'controls'} placeholder='Email' bsStyle={emailInputBsStyle} onBlur={this.onBlurEmail} onKeyDown={this.onKeyDownEmail} required autoFocus />
-                {invalidEmail ? <div className='validateDivAfterRBC'>Bitte Email prüfen</div> : ''}
+            <form
+              className={'form'}
+              autoComplete='off'>
+              <p className='anmelden'>
+                Für diese Funktion müssen Sie angemeldet sein.<br/>
+                <a href='mailto:alex@gabriel-software.ch'>Mailen Sie mir</a>, um ein Login zu erhalten.
+              </p>
+              <div
+                className='formGroup'>
+                <Input
+                  type='email'
+                  id='email'
+                  label={'Email'}
+                  bsSize='small'
+                  className={'controls'}
+                  placeholder='Email'
+                  bsStyle={emailInputBsStyle}
+                  onBlur={this.onBlurEmail}
+                  onKeyDown={this.onKeyDownEmail}
+                  required
+                  autoFocus />
+                {
+                  invalidEmail
+                  ? <div
+                      className='validateDivAfterRBC'>
+                      Bitte Email prüfen
+                    </div>
+                  : null
+                }
               </div>
-              <div className='formGroup'>
-                <Input type='password' id='password' label={'Passwort'} className={'controls'} placeholder='Passwort' bsStyle={passwordInputBsStyle} onBlur={this.onBlurPassword} onKeyDown={this.onKeyDownPassword} required />
-                {invalidPassword ? <div className='validateDivAfterRBC'>Bitte Passwort prüfen</div> : ''}
+              <div
+                className='formGroup'>
+                <Input
+                  type='password'
+                  id='password'
+                  label={'Passwort'}
+                  className={'controls'}
+                  placeholder='Passwort'
+                  bsStyle={passwordInputBsStyle}
+                  onBlur={this.onBlurPassword}
+                  onKeyDown={this.onKeyDownPassword}
+                  required />
+                {
+                  invalidPassword
+                  ? <div
+                      className='validateDivAfterRBC'>
+                      Bitte Passwort prüfen
+                    </div>
+                  : null
+                }
               </div>
-              {loginError ? <Alert bsStyle='danger' onDismiss={this.onAlertDismiss} style={styleAlert}>Fehler beim Anmelden: {loginError}</Alert> : ''}
-              <p className='Passwort' style={{'marginBottom': -5 + 'px'}}>Passwort vergessen?<br/><a href='mailto:alex@gabriel-software.ch'>Mailen Sie mir</a>, möglichst mit derselben email-Adresse, die Sie für das Konto verwenden.</p>
+              {
+                loginError
+                ? <Alert
+                    bsStyle='danger'
+                    onDismiss={this.onAlertDismiss}
+                    style={styleAlert}>
+                    Fehler beim Anmelden: {loginError}
+                  </Alert>
+                : null
+              }
+              <p
+                className='Passwort'
+                style={{'marginBottom': -5 + 'px'}}>
+                Passwort vergessen?<br/>
+                <a href='mailto:alex@gabriel-software.ch'>Mailen Sie mir</a>, möglichst mit derselben email-Adresse, die Sie für das Konto verwenden.
+              </p>
             </form>
           </Modal.Body>
 
           <Modal.Footer>
-            <Button ref='anmeldenButton' className='btn-primary' onClick={this.onClickLogin}>anmelden</Button>
-            <Button onClick={this.schliessen}>schliessen</Button>
+            <Button
+              ref='anmeldenButton'
+              className='btn-primary'
+              onClick={this.onClickLogin}>
+              anmelden
+            </Button>
+            <Button
+              onClick={this.schliessen}>
+              schliessen
+            </Button>
           </Modal.Footer>
 
         </Modal.Dialog>
