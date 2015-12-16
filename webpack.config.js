489022a2fa5a3c44d20916357de66069999d4b3b
Alexander Gabriel <alex@gabriel-software.ch>
2015-12-16 17:35:38 +0100

    update hjs-webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,6 @@ var config = getConfig({
 })
 
 config.module.loaders.push(
-  // bootstraps font-awesome files need this to load in webpack
-  // the url-loader uses DataUrls.
-  // the file-loader emits files.
-  {test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/font-woff'},
-  {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
-  {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-  {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=image/svg+xml'},
   {
     test: /\.jsx?$/,
     exclude: /(node_modules)/,
