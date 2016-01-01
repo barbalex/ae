35705b0259c0c2306104513edbdfa8785989d16d
Alexander Gabriel <alex.barbalex@gmail.com>
2016-01-01 10:41:38 +0100

    remove previous path

--- src/modules/changePathOfObjectInLocalDb.js
+++ src/modules/changePathOfObjectInLocalDb.js
@@ -7,6 +7,7 @@
 'use strict'
 
 import app from 'ampersand-app'
+import { findKey } from 'lodash'
 import getPathFromObject from './getPathFromObject.js'
 
 export default (object) => {
@@ -15,6 +16,10 @@ export default (object) => {
     if (path.length > 0) {
       app.localDb.get('_local/paths')
         .then((doc) => {
+          // remove previous path
+          const previousPath = findKey(doc.paths, (value) => value === object._id)
+          if (previousPath) delete doc[previousPath]
+          // add new path
           doc.paths[path] = object._id
           app.localDb.put(doc)
             .then(() => resolve(doc.paths))
