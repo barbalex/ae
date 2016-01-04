c2bddb3855b33048b2537ce2cf80df2b6c7d841a
Alexander Gabriel <alex@gabriel-software.ch>
2016-01-04 12:02:06 +0100

    work on rebuilding hierarchy

--- src/modules/rebuildHierarchy.js
+++ src/modules/rebuildHierarchy.js
@@ -3,8 +3,18 @@
 import app from 'ampersand-app'
 
 export default () => {
+  let objects = []
   app.objectStore.getObjects()
-    .then((objects) => {
+    .then((items) => {
+      objects = items
+      return true
+    })
+    .then(() => app.localDb.get('_local/groupsLoaded'))
+    .then((doc) => {
+      doc.hierarchy = []
+      return app.localDb.put(doc)
+    })
+    .then(() => {
       // TODO: empty filter options
       app.Actions.loadFilterOptions(objects)
       // TODO: empty path
