7c2ebffc58ce42035b79bd8e94087310e664155d
Alexander Gabriel <alex@gabriel-software.ch>
2016-01-04 14:43:56 +0100

    clean code

--- src/actions.js
+++ src/actions.js
@@ -28,7 +28,6 @@ export default () => {
     loadActivePath: {},
     login: {},
     showGroupLoading: {},
-    showReloadingObjectDerivedData: {},
     queryTaxonomyCollections: {},
     queryPropertyCollections: {},
     queryRelationCollections: {},
