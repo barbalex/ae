e71a5d5f470dce01fbbdd7c1572157fd66ba4b0e
Alexander Gabriel <alex@gabriel-software.ch>
2016-01-31 11:33:19 +0100

    use babelrc

--- webpack.config.js
+++ webpack.config.js
@@ -16,10 +16,7 @@ config.module.loaders.push(
   {
     test: /\.jsx?$/,
     exclude: /(node_modules)/,
-    loader: 'babel',
-    query: {
-      presets: ['es2015', 'react', 'stage-2', 'react-hmre']
-    }
+    loader: 'babel'
   }
 )
 
