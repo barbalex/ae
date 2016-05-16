3546fcd59edc69968aad4dd85640e11bf3042f5c
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-16 21:38:17 +0200

    comply to style

--- src/components/main/importPc/panel4/panel4.js
+++ src/components/main/importPc/panel4/panel4.js
@@ -7,68 +7,81 @@ import ProgressbarImport from '../progressbarImport.js'
 import AlertFirst5Imported from '../alertFirst5Imported.js'
 import AlertFirst5Deleted from '../alertFirst5Deleted.js'
 
-export default React.createClass({
-  displayName: 'Panel4',
+const Panel4 = ({
+  replicatingToAe,
+  replicatingToAeTime,
+  onClickRemovePcInstances,
+  onClickImportieren,
+  name,
+  pcsRemoved,
+  idsOfAeObjects,
+  idsNotImportable,
+  importingProgress,
+  deletingPcInstancesProgress
+}) => (
+  <div>
+    <Button
+      className="btn-primary"
+      onClick={onClickImportieren}
+    >
+      <Glyphicon glyph="download-alt" />&npsb;Eigenschaftensammlung "{name}" importieren
+    </Button>
+    <ButtonDeletePcInstances
+      name={name}
+      pcsRemoved={pcsRemoved}
+      deletingPcInstancesProgress={deletingPcInstancesProgress}
+      onClickRemovePcInstances={onClickRemovePcInstances}
+    />
+    {
+      importingProgress !== null &&
+      !pcsRemoved &&
+      <ProgressbarImport
+        importingProgress={importingProgress}
+      />
+    }
+    {
+      importingProgress === 100 &&
+      !pcsRemoved &&
+      <AlertFirst5Imported
+        idsOfAeObjects={idsOfAeObjects}
+        idsNotImportable={idsNotImportable}
+        replicatingToAe={replicatingToAe}
+        replicatingToAeTime={replicatingToAeTime}
+      />
+    }
+    {
+      deletingPcInstancesProgress !== null &&
+      <ProgressBar
+        bsStyle="success"
+        now={deletingPcInstancesProgress}
+        label={`${deletingPcInstancesProgress}% entfernt`}
+      />
+    }
+    {
+      deletingPcInstancesProgress === 100 &&
+      <AlertFirst5Deleted
+        idsOfAeObjects={idsOfAeObjects}
+        nameBestehend={name}
+        replicatingToAe={replicatingToAe}
+        replicatingToAeTime={replicatingToAeTime}
+      />
+    }
+  </div>
+)
 
-  propTypes: {
-    name: React.PropTypes.string,
-    pcsRemoved: React.PropTypes.bool,
-    idsOfAeObjects: React.PropTypes.array,
-    idsNotImportable: React.PropTypes.array,
-    importingProgress: React.PropTypes.number,
-    deletingPcInstancesProgress: React.PropTypes.number,
-    replicatingToAe: React.PropTypes.string,
-    replicatingToAeTime: React.PropTypes.string,
-    onClickRemovePcInstances: React.PropTypes.func,
-    onClickImportieren: React.PropTypes.func
-  },
+Panel4.displayName = 'Panel4'
 
-  render() {
-    const { replicatingToAe, replicatingToAeTime, onClickRemovePcInstances, onClickImportieren, name, pcsRemoved, idsOfAeObjects, idsNotImportable, importingProgress, deletingPcInstancesProgress } = this.props
-    const showProgressbarImport = importingProgress !== null && !pcsRemoved
-    const showAlertFirst5Imported = importingProgress === 100 && !pcsRemoved
+Panel4.propTypes = {
+  name: React.PropTypes.string,
+  pcsRemoved: React.PropTypes.bool,
+  idsOfAeObjects: React.PropTypes.array,
+  idsNotImportable: React.PropTypes.array,
+  importingProgress: React.PropTypes.number,
+  deletingPcInstancesProgress: React.PropTypes.number,
+  replicatingToAe: React.PropTypes.string,
+  replicatingToAeTime: React.PropTypes.string,
+  onClickRemovePcInstances: React.PropTypes.func,
+  onClickImportieren: React.PropTypes.func
+}
 
-    return (
-      <div>
-        <Button
-          className='btn-primary'
-          onClick={onClickImportieren}>
-            <Glyphicon glyph='download-alt'/> Eigenschaftensammlung "{name}" importieren
-        </Button>
-        <ButtonDeletePcInstances
-          name={name}
-          pcsRemoved={pcsRemoved}
-          deletingPcInstancesProgress={deletingPcInstancesProgress}
-          onClickRemovePcInstances={onClickRemovePcInstances} />
-        {
-          showProgressbarImport &&
-          <ProgressbarImport
-            importingProgress={importingProgress} />
-        }
-        {
-          showAlertFirst5Imported &&
-          <AlertFirst5Imported
-            idsOfAeObjects={idsOfAeObjects}
-            idsNotImportable={idsNotImportable}
-            replicatingToAe={replicatingToAe}
-            replicatingToAeTime={replicatingToAeTime} />
-        }
-        {
-          deletingPcInstancesProgress !== null &&
-          <ProgressBar
-            bsStyle='success'
-            now={deletingPcInstancesProgress}
-            label={`${deletingPcInstancesProgress}% entfernt`} />
-        }
-        {
-          deletingPcInstancesProgress === 100 &&
-          <AlertFirst5Deleted
-            idsOfAeObjects={idsOfAeObjects}
-            nameBestehend={name}
-            replicatingToAe={replicatingToAe}
-            replicatingToAeTime={replicatingToAeTime} />
-        }
-      </div>
-    )
-  }
-})
+export default Panel4
