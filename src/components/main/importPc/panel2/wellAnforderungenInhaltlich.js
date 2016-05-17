ca3829623dac5f13781ec1ca2970ba447a9d96ad
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-17 14:37:51 +0200

    comply to style

--- src/components/main/importPc/panel2/wellAnforderungenInhaltlich.js
+++ src/components/main/importPc/panel2/wellAnforderungenInhaltlich.js
@@ -24,25 +24,63 @@ export default React.createClass({
   render() {
     const { visible } = this.state
     const wellStyle = {
-      marginBottom: 15 + 'px !important'
+      marginBottom: `${15}px !important`
     }
 
     return (
       <Well
-        bsSize='small'
-        style={wellStyle}>
-        <b>Inhaltliche Anforderungen </b> <a href='#' onClick={this.onClickToggle} className='showNextHidden'>{visible ? '...weniger' : '...mehr'}</a>
+        bsSize="small"
+        style={wellStyle}
+      >
+        <b>Inhaltliche Anforderungen</b>
+        &nbsp;
+        <a
+          href="#"
+          onClick={this.onClickToggle}
+          className="showNextHidden"
+        >
+          {visible ? '...weniger' : '...mehr'}
+        </a>
         <ul
-          className='adb-hidden'
-          style={{'display': visible ? 'block' : 'none'}}>
-          <li>Um die Art oder den Lebensraum zu identifizieren müssen Sie eine ID mitliefern.<br/>
-            Entweder die GUID der ArtenDb.<br/>
-            Oder die vom entsprechenden nationalen Artdatenzentrum für diese Artengruppe verwendete ID (z.B. Flora: SISF-Nr)</li>
-          <li>Sie haben bloss eine andere ID? Vielleicht finden Sie sie in einer Eigenschaftensammlung der ArtenDb. Beispielsweise enthält die Flora Indicativa (= "CH Zeigerwerte (2010)") mehrere weitere ID`s aus anderen Florenwerken. Sie können diese Daten gemeinsam mit der GUID der ArtenDb exportieren (z.B. ins Excel-Arbeitsblatt "ID-Liste"). Danach können Sie z.B. in Excel in der Spalte neben ihrer ID mithilfe der Funktion "SVERWEIS" die jeweilige GUID der ArtenDb einfügen. Die Funktion "SVERWEIS" schlägt dabei in der "ID-Liste" für ihre ID die GUID nach</li>
-          <li>Achten Sie bitte darauf, die Feldnamen und die enthaltenen Werte uncodiert und aussagekräftig zu gestalten. Auch Nutzer ohne Spezialwissen sollten sie verstehen können</li>
-          <li>Sind für Ihre Daten Codierungen wichtig? Dann importieren Sie die Daten doch einfach in zwei Felder: Eines codiert, das andere uncodiert. Und ergänzen Sie Ihre Feldnamen mit den Zusätzen "codiert" bzw. "uncodiert"</li>
-          <li>ArtenDb setzt beim Export von Daten den Namen der Eigenschaftensammlung vor den Feldnamen, z.B.: "ZH FNS (aktuell): Artwert". Das erleichtert das Verständnis der Daten in der "flachen" Tabelle.<br/>
-            Beim Import hingegen ist es NICHT sinnvoll, die Felder so zu nennen. Denn den Namen der Eigenschaftensammlung haben Sie im letzten Arbeitsschritt schon erfasst und er wird den Daten beim Import in hierarchischer Form mitgegeben</li>
+          className="adb-hidden"
+          style={{ display: visible ? 'block' : 'none' }}
+        >
+          <li>
+            Um die Art oder den Lebensraum zu identifizieren müssen Sie eine <strong>ID</strong> mitliefern.<br />
+            Entweder die GUID der ArtenDb.<br />
+            Oder die vom entsprechenden nationalen Artdatenzentrum
+            für diese Artengruppe verwendete ID (z.B. Flora: SISF-Nr)
+          </li>
+          <li>
+            Sie haben bloss eine <strong>andere ID?</strong><br />
+            Vielleicht finden Sie sie in einer Eigenschaftensammlung der ArtenDb.<br />
+            Beispielsweise enthält die Flora Indicativa
+            (= "CH Zeigerwerte (2010)") mehrere weitere ID`s aus anderen Florenwerken.<br />
+            Sie können diese Daten gemeinsam mit der GUID der ArtenDb exportieren
+            (z.B. ins Excel-Arbeitsblatt "ID-Liste").<br />
+            Danach können Sie z.B. in Excel in der Spalte neben ihrer ID mithilfe der Funktion "SVERWEIS" die jeweilige
+            GUID der ArtenDb einfügen.<br />
+            Die Funktion "SVERWEIS" schlägt dabei in der "ID-Liste" für ihre ID die GUID nach
+          </li>
+          <li>
+            Achten Sie bitte darauf, die <strong>Feldnamen und</strong> die enthaltenen <strong>Werte
+            uncodiert und aussagekräftig</strong> zu gestalten.<br />
+            Auch Nutzer ohne Spezialwissen sollten sie verstehen können
+          </li>
+          <li>
+            Sind für Ihre Daten <strong>Codierungen</strong> wichtig?<br />
+            Dann importieren Sie die Daten doch einfach in zwei Felder:<br />
+            Eines codiert, das andere uncodiert.<br />
+            Und ergänzen Sie Ihre Feldnamen mit den Zusätzen "codiert" bzw. "uncodiert"
+          </li>
+          <li>
+            ArtenDb setzt beim Export von Daten den
+            <strong>Namen der Eigenschaftensammlung vor den Feldnamen</strong>,<br />
+            z.B.: "ZH FNS (aktuell): Artwert". Das erleichtert das Verständnis der Daten in der "flachen" Tabelle.<br />
+            Beim Import hingegen ist es NICHT sinnvoll, die Felder so zu nennen.<br />
+            Denn den Namen der Eigenschaftensammlung haben Sie im letzten Arbeitsschritt schon erfasst
+            und er wird den Daten beim Import in hierarchischer Form mitgegeben
+          </li>
         </ul>
       </Well>
     )
--- src/components/main/importPc/panel2/wellTechnAnforderungenAnDatei.js
+++ src/components/main/importPc/panel2/wellTechnAnforderungenAnDatei.js
@@ -25,26 +25,37 @@ export default React.createClass({
     const { visible } = this.state
 
     return (
-      <Well
-        bsSize='small'>
-        <b>Anforderungen an die Datei</b> <a href='#' onClick={this.onClickToggle} className='showNextHidden'>{visible ? '...weniger' : '...mehr'}</a>
+      <Well bsSize="small">
+        <b>Anforderungen an die Datei</b>
+        &nbsp;
+        <a
+          href="#"
+          onClick={this.onClickToggle}
+          className="showNextHidden"
+        >
+          {visible ? '...weniger' : '...mehr'}
+        </a>
         <ul
-          className='adb-hidden'
-          style={{'display': visible ? 'block' : 'none'}}>
+          className="adb-hidden"
+          style={{ display: visible ? 'block' : 'none' }}
+        >
           <li>
-            Sie können Dateien vom Typ <b>.csv</b> (kommagetrennte Textdatei) oder <b>.xlsx</b> (Excel-Datei) importieren
+            Sie können Dateien vom Typ <b>.csv</b> (kommagetrennte Textdatei)
+            oder <b>.xlsx</b> (Excel-Datei) importieren
           </li>
           <li>
             Die erste Zeile enthält die Feldnamen
           </li>
           <li>
-            Die weiteren Zeilen enthalten je einen Datensatz, d.h. die Eigenschaften für die betreffende Art oder den Lebensraum
+            Die weiteren Zeilen enthalten je einen Datensatz,
+            d.h. die Eigenschaften für die betreffende Art oder den Lebensraum
           </li>
           <li>
             Alle Zeilen sollten dieselbe Anzahl Felder bzw. Spalten enthalten
           </li>
           <li>
-            Achten Sie darauf, dass die Datei die Codierung "UTF 8" benutzt. Nur in diesem Format werden Umlaute und Sonderzeichen vollständig erkannt
+            Achten Sie darauf, dass die Datei die Codierung "UTF 8" benutzt.
+            Nur in diesem Format werden Umlaute und Sonderzeichen vollständig erkannt
           </li>
         </ul>
       </Well>
