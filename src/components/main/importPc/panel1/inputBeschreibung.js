2ef960c664e2723407dab64320bd4085911df01b
Alexander Gabriel <alex.barbalex@gmail.com>
2016-05-18 13:52:37 +0200

    comply to style

--- src/components/main/importPc/panel1/inputBeschreibung.js
+++ src/components/main/importPc/panel1/inputBeschreibung.js
@@ -3,66 +3,66 @@
 import React from 'react'
 import { OverlayTrigger, Popover } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'InputBeschreibung',
+const popover = (
+  <Popover
+    id="InputBeschreibungPopover"
+    title="So beschreiben Sie die Sammlung:"
+  >
+    <p>
+      Die Beschreibung sollte im ersten Teil etwa einem klassischen Literaturzitat entsprechen.<br />
+      Beispiel: "Gigon A. et al. (1998): Blaue Listen der erfolgreich erhaltenen oder geförderten Tier-
+      und Pflanzenarten der Roten Listen. Methodik und Anwendung in der nördlichen Schweiz.
+      Veröff. Geobot. Inst. ETH, Stiftung Rübel, Zürich 129: 1-137 + 180 pp. Appendicesn".
+    </p>
+    <p>
+      In einem zweiten Teil sollte beschrieben werden, welche Informationen
+      die Eigenschaftensammlung enthält.<br />
+      Beispiel: "Eigenschaften von 207 Tierarten und 885 Pflanzenarten".
+    </p>
+    <p>
+      Es kann sehr nützlich sein, zu wissen, wozu die Informationen zusammengestellt wurden.
+    </p>
+  </Popover>
+)
 
-  propTypes: {
-    beschreibung: React.PropTypes.string,
-    validBeschreibung: React.PropTypes.bool,
-    onChangeBeschreibung: React.PropTypes.func
-  },
-
-  onChange(event) {
-    const beschreibung = event.target.value
-    // inform parent component
-    this.props.onChangeBeschreibung(beschreibung)
-  },
-
-  popover() {
-    return (
-      <Popover
-        id='InputBeschreibungPopover'
-        title='So beschreiben Sie die Sammlung:'>
-        <p>Die Beschreibung sollte im ersten Teil etwa einem klassischen Literaturzitat entsprechen.<br/>
-          Beispiel: "Gigon A. et al. (1998): Blaue Listen der erfolgreich erhaltenen oder geförderten Tier- und Pflanzenarten der Roten Listen. Methodik und Anwendung in der nördlichen Schweiz. Veröff. Geobot. Inst. ETH, Stiftung Rübel, Zürich 129: 1-137 + 180 pp. Appendicesn".</p>
-        <p>In einem zweiten Teil sollte beschrieben werden, welche Informationen die Eigenschaftensammlung enthält.<br/>
-          Beispiel: "Eigenschaften von 207 Tierarten und 885 Pflanzenarten".</p>
-        <p>Es kann sehr nützlich sein, zu wissen, wozu die Informationen zusammengestellt wurden.</p>
-      </Popover>
-    )
-  },
-
-  render() {
-    const { beschreibung, validBeschreibung } = this.props
-
-    return (
-      <div
-        className={validBeschreibung ? 'form-group' : 'form-group has-error'}>
-        <OverlayTrigger
-          trigger={['click', 'focus']}
-          rootClose
-          placement='right'
-          overlay={this.popover()}>
-          <label
-            className='control-label withPopover'>
-            Beschreibung
-          </label>
-        </OverlayTrigger>
-        <input
-          type='textarea'
-          className='form-control controls'
-          value={beschreibung}
-          onChange={this.onChange}
-          rows={1} />
-        {
-          validBeschreibung
-          ? null
-          : <div
-              className='validateDiv feld'>
-              Eine Beschreibung ist erforderlich
-            </div>
-        }
+const InputBeschreibung = ({ beschreibung, validBeschreibung, onChangeBeschreibung }) => (
+  <div
+    className={validBeschreibung ? 'form-group' : 'form-group has-error'}
+  >
+    <OverlayTrigger
+      trigger={['click', 'focus']}
+      rootClose
+      placement="right"
+      overlay={popover}
+    >
+      <label
+        className="control-label withPopover"
+      >
+        Beschreibung
+      </label>
+    </OverlayTrigger>
+    <input
+      type="textarea"
+      className="form-control controls"
+      value={beschreibung}
+      onChange={(event) => onChangeBeschreibung(event.target.value)}
+      rows={1}
+    />
+    {
+      !validBeschreibung &&
+      <div className="validateDiv feld">
+        Eine Beschreibung ist erforderlich
       </div>
-    )
-  }
-})
+    }
+  </div>
+)
+
+InputBeschreibung.displayName = 'InputBeschreibung'
+
+InputBeschreibung.propTypes = {
+  beschreibung: React.PropTypes.string,
+  validBeschreibung: React.PropTypes.bool,
+  onChangeBeschreibung: React.PropTypes.func
+}
+
+export default InputBeschreibung
--- src/components/main/importPc/panel1/inputDatenstand.js
+++ src/components/main/importPc/panel1/inputDatenstand.js
@@ -3,62 +3,51 @@
 import React from 'react'
 import { OverlayTrigger, Popover } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'InputDatenstand',
+const popover = (
+  <Popover
+    id="InputDatenstandPopover"
+    title="Wozu ein Datenstand?"
+  >
+    <p>Hier sieht der Nutzer, wann die Eigenschaftensammlung zuletzt aktualisiert wurde.</p>
+  </Popover>
+)
 
-  propTypes: {
-    datenstand: React.PropTypes.string,
-    validDatenstand: React.PropTypes.bool,
-    onChangeDatenstand: React.PropTypes.func
-  },
-
-  onChange(event) {
-    const datenstand = event.target.value
-    // inform parent component
-    this.props.onChangeDatenstand(datenstand)
-  },
-
-  popover() {
-    return (
-      <Popover
-        id='InputDatenstandPopover'
-        title='Wozu ein Datenstand?'>
-        <p>Hier sieht der Nutzer, wann die Eigenschaftensammlung zuletzt aktualisiert wurde.</p>
-      </Popover>
-    )
-  },
-
-  render() {
-    const { datenstand, validDatenstand } = this.props
-
-    return (
-      <div
-        className={validDatenstand ? 'form-group' : 'form-group has-error'}>
-        <OverlayTrigger
-          trigger={['click', 'focus']}
-          rootClose
-          placement='right'
-          overlay={this.popover()}>
-          <label
-            className='control-label withPopover'>
-            Datenstand
-          </label>
-        </OverlayTrigger>
-        <input
-          type='textarea'
-          className='form-control controls'
-          rows={1}
-          value={datenstand}
-          onChange={this.onChange} />
-        {
-          validDatenstand
-          ? null
-          : <div
-              className='validateDiv feld'>
-              Ein Datenstand ist erforderlich
-            </div>
-        }
+const InputDatenstand = ({ datenstand, validDatenstand, onChangeDatenstand }) => (
+  <div
+    className={validDatenstand ? 'form-group' : 'form-group has-error'}
+  >
+    <OverlayTrigger
+      trigger={['click', 'focus']}
+      rootClose
+      placement="right"
+      overlay={popover}
+    >
+      <label className="control-label withPopover">
+        Datenstand
+      </label>
+    </OverlayTrigger>
+    <input
+      type="textarea"
+      className="form-control controls"
+      rows={1}
+      value={datenstand}
+      onChange={(event) => onChangeDatenstand(event.target.value)}
+    />
+    {
+      validDatenstand &&
+      <div className="validateDiv feld">
+        Ein Datenstand ist erforderlich
       </div>
-    )
-  }
-})
+    }
+  </div>
+)
+
+InputDatenstand.displayName = 'InputDatenstand'
+
+InputDatenstand.propTypes = {
+  datenstand: React.PropTypes.string,
+  validDatenstand: React.PropTypes.bool,
+  onChangeDatenstand: React.PropTypes.func
+}
+
+export default InputDatenstand
