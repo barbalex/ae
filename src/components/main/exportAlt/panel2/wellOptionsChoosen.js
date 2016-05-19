d069e65b773f1c09b71224891c3a3d0f78758497
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-19 14:10:33 +0200

    comply with style

--- src/components/main/exportAlt/panel2/wellOptionsChoosen.js
+++ src/components/main/exportAlt/panel2/wellOptionsChoosen.js
@@ -2,83 +2,80 @@
 
 import React from 'react'
 import { Well } from 'react-bootstrap'
-import _ from 'lodash'
 
 const ulStyle = {
   paddingLeft: 20,
   marginBottom: 3
 }
 
-export default React.createClass({
-  displayName: 'OptionsChoosen',
+const pStyle = {
+  marginBottom: 0,
+  fontWeight: 'bold'
+}
 
-  propTypes: {
-    urlOptions: React.PropTypes.object,
-    includeDataFromSynonyms: React.PropTypes.bool,
-    oneRowPerRelation: React.PropTypes.bool
-  },
+const oneRowPerRelationText = (oneRowPerRelation) => (
+  oneRowPerRelation ?
+  'Pro Beziehung eine Zeile' :
+  'Pro Art/Lebensraum eine Zeile, alle Beziehungen kommagetrennt in einem Feld'
+)
 
-  groupsText () {
-    const { urlOptions } = this.props
-    const groups = urlOptions.object.Gruppen.value
-    const prefix = groups.length > 1 ? 'Gruppen: ' : 'Gruppe: '
-    return prefix + groups.join(', ')
-  },
+const dataFromSynonymsText = (includeDataFromSynonyms) => (
+  includeDataFromSynonyms ?
+  'Informationen von Synonymen werden berücksichtigt' :
+  'Informationen von Synonymen werden ignoriert'
+)
 
-  dataFromSynonymsText () {
-    const { includeDataFromSynonyms } = this.props
-    return includeDataFromSynonyms ? 'Informationen von Synonymen werden berücksichtigt' : 'Informationen von Synonymen werden ignoriert'
-  },
+const groupsText = (urlOptions) => {
+  const groups = urlOptions.object.Gruppen.value
+  const prefix = groups.length > 1 ? 'Gruppen: ' : 'Gruppe: '
+  return prefix + groups.join(', ')
+}
 
-  oneRowPerRelationText () {
-    const { oneRowPerRelation } = this.props
-    return oneRowPerRelation ? 'Pro Beziehung eine Zeile' : 'Pro Art/Lebensraum eine Zeile, alle Beziehungen kommagetrennt in einem Feld'
-  },
-
-  fields () {
-    const { urlOptions } = this.props
-    let fields = []
-    Object.keys(urlOptions).forEach((cName) => {
-      Object.keys(urlOptions[cName]).forEach((fName) => {
-        const field = urlOptions[cName][fName]
-        if (field.export) fields.push({ cName, fName })
-      })
+const fields = (urlOptions) => {
+  const myFields = []
+  Object.keys(urlOptions).forEach((cName) => {
+    Object.keys(urlOptions[cName]).forEach((fName) => {
+      const field = urlOptions[cName][fName]
+      if (field.export) myFields.push({ cName, fName })
     })
-    return fields
-  },
+  })
+  return myFields
+}
 
-  fieldsList () {
-    const fields = this.fields()
-    if (fields.length > 0) {
-      return fields.map((field, index) => {
-        const fName = field.fName
-        const cName = field.cName
-        if (cName === 'object') return <li key={index}>{fName === '_id' ? 'GUID' : fName}</li>
-        return <li key={index}>{cName}: {fName}</li>
-      })
-    }
-    return <li>Keine Eigenschaft gewählt</li>
-  },
-
-  render() {
-    const pStyle = {
-      marginBottom: 0,
-      fontWeight: 'bold'
-    }
-    return (
-      <Well bsSize='small'>
-        <p style={pStyle}>Gewählte Optionen:</p>
-        <ul style={ulStyle}>
-          <li>{this.groupsText()}</li>
-          <li>{this.dataFromSynonymsText()}</li>
-          <li>{this.oneRowPerRelationText()}</li>
-          <li>Eigenschaften:
-            <ul style={ulStyle}>
-              {this.fieldsList()}
-            </ul>
-          </li>
-        </ul>
-      </Well>
-    )
+const fieldsList = (urlOptions) => {
+  const myFields = fields(urlOptions)
+  if (myFields.length > 0) {
+    return myFields.map((field, index) => {
+      const fName = field.fName
+      const cName = field.cName
+      if (cName === 'object') return <li key={index}>{fName === '_id' ? 'GUID' : fName}</li>
+      return <li key={index}>{cName}: {fName}</li>
+    })
   }
-})
+  return <li>Keine Eigenschaft gewählt</li>
+}
+
+const WellOptionsChoosen = ({ includeDataFromSynonyms, oneRowPerRelation, urlOptions }) =>
+  <Well bsSize="small">
+    <p style={pStyle}>Gewählte Optionen:</p>
+    <ul style={ulStyle}>
+      <li>{groupsText(urlOptions)}</li>
+      <li>{dataFromSynonymsText(includeDataFromSynonyms)}</li>
+      <li>{oneRowPerRelationText(oneRowPerRelation)}</li>
+      <li>Eigenschaften:
+        <ul style={ulStyle}>
+          {fieldsList(urlOptions)}
+        </ul>
+      </li>
+    </ul>
+  </Well>
+
+WellOptionsChoosen.displayName = 'WellOptionsChoosen'
+
+WellOptionsChoosen.propTypes = {
+  urlOptions: React.PropTypes.object,
+  includeDataFromSynonyms: React.PropTypes.bool,
+  oneRowPerRelation: React.PropTypes.bool
+}
+
+export default WellOptionsChoosen
--- src/components/main/importPc/panel1/inputOrgMitSchreibrecht.js
+++ src/components/main/importPc/panel1/inputOrgMitSchreibrecht.js
@@ -5,8 +5,8 @@ import { OverlayTrigger, Popover } from 'react-bootstrap'
 
 const popover = (
   <Popover
-    id='inputOrganisationMitSchreibrechtPopover'
-    title='Was heisst das?'
+    id="inputOrganisationMitSchreibrechtPopover"
+    title="Was heisst das?"
   >
     <p>Diese Organisation verwaltet die Eigenschaftensammlung.</p>
     <p>Sie bestimmt, wer sie verändern kann, bzw. wer importieren kann.</p>
@@ -26,7 +26,7 @@ const options = (userIsEsWriterInOrgs) => {
     // add an empty option at the beginning
     myOptions.unshift(
       <option
-        key='noValue'
+        key="noValue"
         value={null}
       >
       </option>
@@ -40,49 +40,47 @@ const options = (userIsEsWriterInOrgs) => {
   )
 }
 
-export default React.createClass({
-  displayName: 'InputOrganisationMitSchreibrecht',
-
-  propTypes: {
-    orgMitSchreibrecht: React.PropTypes.string,
-    validOrgMitSchreibrecht: React.PropTypes.bool,
-    onChangeOrgMitSchreibrecht: React.PropTypes.func,
-    userIsEsWriterInOrgs: React.PropTypes.array
-  },
-
-  render() {
-    const { orgMitSchreibrecht, onChangeOrgMitSchreibrecht, userIsEsWriterInOrgs, validOrgMitSchreibrecht } = this.props
-    // const selected = userIsEsWriterInOrgs && userIsEsWriterInOrgs.length === 1 ? userIsEsWriterInOrgs[0] : null
-
-    return (
-      <div
-        className={validOrgMitSchreibrecht ? 'form-group' : 'form-group has-error'}>
-        <OverlayTrigger
-          trigger={['click', 'focus']}
-          rootClose
-          placement='right'
-          overlay={popover}>
-          <label
-            className='control-label withPopover'>
-            Organisation mit Schreibrecht
-          </label>
-        </OverlayTrigger>
-        <select
-          className='form-control controls'
-          value={orgMitSchreibrecht}
-          onChange={onChangeOrgMitSchreibrecht}
-        >
-          {options(userIsEsWriterInOrgs)}
-        </select>
-        {
-          validOrgMitSchreibrecht
-          ? null
-          : <div
-              className='validateDiv feld'>
-              Es muss eine Organisation mit Schreibrecht gewählt sein
-            </div>
-        }
+const InputOrgMitSchreibrecht = ({
+  orgMitSchreibrecht,
+  onChangeOrgMitSchreibrecht,
+  userIsEsWriterInOrgs,
+  validOrgMitSchreibrecht
+}) =>
+  <div
+    className={validOrgMitSchreibrecht ? 'form-group' : 'form-group has-error'}
+  >
+    <OverlayTrigger
+      trigger={['click', 'focus']}
+      rootClose
+      placement="right"
+      overlay={popover}
+    >
+      <label className="control-label withPopover">
+        Organisation mit Schreibrecht
+      </label>
+    </OverlayTrigger>
+    <select
+      className="form-control controls"
+      value={orgMitSchreibrecht}
+      onChange={onChangeOrgMitSchreibrecht}
+    >
+      {options(userIsEsWriterInOrgs)}
+    </select>
+    {
+      !validOrgMitSchreibrecht &&
+      <div className="validateDiv feld">
+        Es muss eine Organisation mit Schreibrecht gewählt sein
       </div>
-    )
-  }
-})
+    }
+  </div>
+
+InputOrgMitSchreibrecht.displayName = 'InputOrgMitSchreibrecht'
+
+InputOrgMitSchreibrecht.propTypes = {
+  orgMitSchreibrecht: React.PropTypes.string,
+  validOrgMitSchreibrecht: React.PropTypes.bool,
+  onChangeOrgMitSchreibrecht: React.PropTypes.func,
+  userIsEsWriterInOrgs: React.PropTypes.array
+}
+
+export default InputOrgMitSchreibrecht
