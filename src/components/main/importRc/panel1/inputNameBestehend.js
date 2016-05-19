f9bee14a680542e4c1459c513d57f03374467d69
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-19 22:49:12 +0200

    comply to style

--- src/components/main/importRc/panel1/inputNameBestehend.js
+++ src/components/main/importRc/panel1/inputNameBestehend.js
@@ -5,87 +5,88 @@ import isUserServerAdmin from '../../../../modules/isUserServerAdmin.js'
 import isUserOrgAdmin from '../../../../modules/isUserOrgAdmin.js'
 import isUserEsWriter from '../../../../modules/isUserEsWriter.js'
 
-export default React.createClass({
-  displayName: 'InputNameBestehend',
-
-  propTypes: {
-    groupsLoadedOrLoading: React.PropTypes.array,
-    nameBestehend: React.PropTypes.string,
-    email: React.PropTypes.string,
-    userRoles: React.PropTypes.array,
-    rcs: React.PropTypes.array,
-    onChangeNameBestehend: React.PropTypes.func,
-    userIsEsWriterInOrgs: React.PropTypes.array
-  },
-
-  onChange(event) {
-    const nameBestehend = event.target.value
-    // pass variables to parent component
-    this.props.onChangeNameBestehend(nameBestehend)
-  },
-
-  options () {
-    const { userRoles, rcs, groupsLoadedOrLoading } = this.props
-    if (rcs && rcs.length > 0) {
-      let options = rcs.map((rc, index) => {
-        const { name, combining, organization } = rc
-        // mutable if user is: esWriter of org, admin of org, db/server-Admin
-        const mutable = isUserServerAdmin(userRoles) || isUserOrgAdmin(userRoles, organization) || isUserEsWriter(userRoles, organization) || combining
-        const className = mutable ? 'adbGruenFett' : 'adbGrauNormal'
-        return (
-          <option
-            key={index}
-            value={name}
-            className={className}>
-            {name}
-          </option>
-        )
-      })
-      // add an empty option at the beginning
-      options.unshift(
-        <option
-          key='noValue'
-          value={null}>
-        </option>
+const options = (userRoles, rcs, groupsLoadedOrLoading) => {
+  if (rcs && rcs.length > 0) {
+    const myOptions = rcs.map((rc, index) => {
+      const { name, combining, organization } = rc
+      // mutable if user is: esWriter of org, admin of org, db/server-Admin
+      const mutable = (
+        isUserServerAdmin(userRoles) ||
+        isUserOrgAdmin(userRoles, organization) ||
+        isUserEsWriter(userRoles, organization) ||
+        combining
       )
-      return options
-    } else if (groupsLoadedOrLoading && groupsLoadedOrLoading.length > 0) {
-      // this option is showed while loading
+      const className = mutable ? 'adbGruenFett' : 'adbGrauNormal'
       return (
         <option
-          value={null}>
-          Lade Daten...
+          key={index}
+          value={name}
+          className={className}
+        >
+          {name}
         </option>
       )
-    } else {
-      return (
-        <option
-          value={null}>
-          Keine Gruppe geladen
-        </option>
-      )
-    }
-  },
-
-  render() {
-    const { nameBestehend } = this.props
-
+    })
+    // add an empty option at the beginning
+    myOptions.unshift(
+      <option
+        key="noValue"
+        value={null}
+      >
+      </option>
+    )
+    return myOptions
+  } else if (groupsLoadedOrLoading && groupsLoadedOrLoading.length > 0) {
+    // this option is showed while loading
     return (
-      <div
-        className='form-group'>
-        <label
-          className='control-label'
-          htmlFor='nameBestehend'>
-          Bestehende wählen
-        </label>
-        <select
-          id='nameBestehend'
-          className='form-control controls'
-          selected={nameBestehend}
-          onChange={this.onChange}>
-          {this.options()}
-        </select>
-      </div>
+      <option value={null}>
+        Lade Daten...
+      </option>
     )
   }
-})
+  return (
+    <option value={null}>
+      Keine Gruppe geladen
+    </option>
+  )
+}
+
+const InputNameBestehend = ({
+  nameBestehend,
+  onChangeNameBestehend,
+  userRoles,
+  rcs,
+  groupsLoadedOrLoading
+}) =>
+  <div className="form-group">
+    <label
+      className="control-label"
+      htmlFor="nameBestehend"
+    >
+      Bestehende wählen
+    </label>
+    <select
+      id="nameBestehend"
+      className="form-control controls"
+      selected={nameBestehend}
+      onChange={(event) =>
+        onChangeNameBestehend(event.target.value)
+      }
+    >
+      {options(userRoles, rcs, groupsLoadedOrLoading)}
+    </select>
+  </div>
+
+InputNameBestehend.displayName = 'InputNameBestehend'
+
+InputNameBestehend.propTypes = {
+  groupsLoadedOrLoading: React.PropTypes.array,
+  nameBestehend: React.PropTypes.string,
+  email: React.PropTypes.string,
+  userRoles: React.PropTypes.array,
+  rcs: React.PropTypes.array,
+  onChangeNameBestehend: React.PropTypes.func,
+  userIsEsWriterInOrgs: React.PropTypes.array
+}
+
+export default InputNameBestehend
--- src/components/main/importRc/panel1/inputOrgMitSchreibrecht.js
+++ src/components/main/importRc/panel1/inputOrgMitSchreibrecht.js
@@ -3,89 +3,100 @@
 import React from 'react'
 import { OverlayTrigger, Popover } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'InputOrganisationMitSchreibrecht',
-
-  propTypes: {
-    validOrgMitSchreibrecht: React.PropTypes.bool,
-    onChangeOrgMitSchreibrecht: React.PropTypes.func,
-    userIsEsWriterInOrgs: React.PropTypes.array
-  },
-
-  popover() {
-    return (
-      <Popover
-        id='inputOrganisationMitSchreibrechtPopover'
-        title='Was heisst das?'>
-        <p>Diese Organisation verwaltet die Beziehungssammlung.</p>
-        <p>Sie bestimmt, wer sie verändern kann, bzw. wer importieren kann.</p>
-      </Popover>
+const options = (userIsEsWriterInOrgs) => {
+  if (userIsEsWriterInOrgs && userIsEsWriterInOrgs.length > 0) {
+    const myOptions = userIsEsWriterInOrgs.map((org, index) => (
+      <option
+        key={index}
+        value={org}
+      >
+        {org}
+      </option>
+    ))
+    // add an empty option at the beginning
+    myOptions.unshift(
+      <option
+        key="noValue"
+        value={null}
+      >
+      </option>
     )
-  },
-
-  options () {
-    const { userIsEsWriterInOrgs } = this.props
-
-    if (userIsEsWriterInOrgs && userIsEsWriterInOrgs.length > 0) {
-      let options = userIsEsWriterInOrgs.map((org, index) => {
-        return (
-          <option
-            key={index}
-            value={org}>
-            {org}
-          </option>
-        )
-      })
-      // add an empty option at the beginning
-      options.unshift(
-        <option
-          key='noValue'
-          value={null}>
-        </option>
-      )
-      return options
-    } else {
-      return (
-        <option
-          value={null}>
-          Keine Organisation geladen
-        </option>
-      )
-    }
-  },
-
-  render() {
-    const { onChangeOrgMitSchreibrecht, userIsEsWriterInOrgs, validOrgMitSchreibrecht } = this.props
-    const selected = userIsEsWriterInOrgs && userIsEsWriterInOrgs.length === 1 ? userIsEsWriterInOrgs[0] : null
-
-    return (
-      <div
-        className={validOrgMitSchreibrecht ? 'form-group' : 'form-group has-error'}>
-        <OverlayTrigger
-          trigger={['click', 'focus']}
-          rootClose
-          placement='right'
-          overlay={this.popover()}>
-          <label
-            className='control-label withPopover'>
-            Organisation mit Schreibrecht
-          </label>
-        </OverlayTrigger>
-        <select
-          className='form-control controls'
-          value={selected}
-          onChange={onChangeOrgMitSchreibrecht}>
-          {this.options()}
-        </select>
-        {
-          validOrgMitSchreibrecht
-          ? null
-          : <div
-              className='validateDiv feld'>
-              Es muss eine Organisation mit Schreibrecht gewählt sein
-            </div>
-        }
-      </div>
-    )
+    return myOptions
   }
-})
+  return (
+    <option value={null}>
+      Keine Organisation geladen
+    </option>
+  )
+}
+
+const popover = () =>
+  <Popover
+    id="inputOrganisationMitSchreibrechtPopover"
+    title="Was heisst das?"
+  >
+    <p>
+      Diese Organisation verwaltet die Beziehungssammlung.
+    </p>
+    <p>
+      Sie bestimmt, wer sie verändern kann, bzw. wer importieren kann.
+    </p>
+  </Popover>
+
+const InputOrganisationMitSchreibrecht = ({
+  onChangeOrgMitSchreibrecht,
+  userIsEsWriterInOrgs,
+  validOrgMitSchreibrecht
+}) => {
+  const selected = (
+    userIsEsWriterInOrgs &&
+    userIsEsWriterInOrgs.length === 1 ?
+    userIsEsWriterInOrgs[0] :
+    null
+  )
+
+  return (
+    <div
+      className={validOrgMitSchreibrecht ? 'form-group' : 'form-group has-error'}
+    >
+      <OverlayTrigger
+        trigger={['click', 'focus']}
+        rootClose
+        placement="right"
+        overlay={popover()}
+      >
+        <label
+          className="control-label withPopover"
+        >
+          Organisation mit Schreibrecht
+        </label>
+      </OverlayTrigger>
+      <select
+        className="form-control controls"
+        value={selected}
+        onChange={onChangeOrgMitSchreibrecht}
+      >
+        {options(userIsEsWriterInOrgs)}
+      </select>
+      {
+        !validOrgMitSchreibrecht &&
+        <div
+          className="validateDiv feld"
+        >
+          Es muss eine Organisation mit Schreibrecht gewählt sein
+        </div>
+      }
+    </div>
+  )
+}
+
+
+InputOrganisationMitSchreibrecht.displayName = 'InputOrganisationMitSchreibrecht'
+
+InputOrganisationMitSchreibrecht.propTypes = {
+  validOrgMitSchreibrecht: React.PropTypes.bool,
+  onChangeOrgMitSchreibrecht: React.PropTypes.func,
+  userIsEsWriterInOrgs: React.PropTypes.array
+}
+
+export default InputOrganisationMitSchreibrecht
