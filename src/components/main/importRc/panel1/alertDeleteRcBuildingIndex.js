47d9b053d2bbd1f534ea69e69bddecc791e6556b
Alexander Gabriel <alex.barbalex@gmail.com>
2016-05-19 21:16:10 +0200

    comply to style

--- src/components/main/importRc/panel1/alertDeleteRcBuildingIndex.js
+++ src/components/main/importRc/panel1/alertDeleteRcBuildingIndex.js
@@ -3,19 +3,21 @@
 import React from 'react'
 import { Alert } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'AlertDeleteRcBuildingIndex',
+const style = {
+  marginBottom: 5,
+  marginTop: 11
+}
 
-  render() {
-    const style = {
-      marginBottom: 5,
-      marginTop: 11
-    }
-    return (
-      <Alert className='feld' bsStyle='info' style={style}>
-        Hole Arten/Lebensräume, um die Eigenschaftensammlung daraus zu löschen.<br/>
-        Beim ersten Mal muss der Index aufgebaut werden. Das dauert einige Minuten...
-      </Alert>
-    )
-  }
-})
+const AlertDeleteRcBuildingIndex = () =>
+  <Alert
+    className="feld"
+    bsStyle="info"
+    style={style}
+  >
+    Hole Arten/Lebensräume, um die Eigenschaftensammlung daraus zu löschen.<br />
+    Beim ersten Mal muss der Index aufgebaut werden. Das dauert einige Minuten...
+  </Alert>
+
+AlertDeleteRcBuildingIndex.displayName = 'AlertDeleteRcBuildingIndex'
+
+export default AlertDeleteRcBuildingIndex
