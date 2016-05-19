c89e450e06266f55e218fcf69a3df869fd7b38e5
Alexander Gabriel <alex.barbalex@gmail.com>
2016-05-19 21:16:05 +0200

    comply to style

--- src/components/main/importPc/panel3/inputImportFields.js
+++ src/components/main/importPc/panel3/inputImportFields.js
@@ -4,50 +4,48 @@ import React from 'react'
 import { union } from 'lodash'
 import { Input } from 'react-bootstrap'
 
-const style = {
-  height: `${(keys.length * 18) + 9}px`
+const InputImportFields = ({ pcsToImport, idsImportIdField, onChangeImportId }) => {
+  // get a list of all keys
+  let keys = []
+  pcsToImport.forEach((pc) => {
+    keys = union(keys, Object.keys(pc))
+  })
+
+  const style = {
+    height: `${(keys.length * 18) + 9}px`
+  }
+
+  const options = keys.map((key, index) => (
+    <option
+      key={index}
+      value={key}
+    >
+      {key}
+    </option>
+  ))
+
+  return (
+    <Input
+      type="select"
+      label="Feld mit eindeutiger ID in den Importdaten"
+      multiple
+      className="form-control controls"
+      style={style}
+      bsSize="small"
+      value={[idsImportIdField]}
+      onChange={(event) => onChangeImportId(event.target.value)}
+    >
+      {options}
+    </Input>
+  )
 }
 
-export default React.createClass({
-  displayName: 'InputImportFields',
+InputImportFields.displayName = 'InputImportFields'
 
-  propTypes: {
-    idsImportIdField: React.PropTypes.string,
-    pcsToImport: React.PropTypes.array,
-    onChangeImportId: React.PropTypes.func
-  },
+InputImportFields.propTypes = {
+  idsImportIdField: React.PropTypes.string,
+  pcsToImport: React.PropTypes.array,
+  onChangeImportId: React.PropTypes.func
+}
 
-  render() {
-    const { pcsToImport, idsImportIdField, onChangeImportId } = this.props
-
-     // get a list of all keys
-    let keys = []
-    pcsToImport.forEach((pc) => {
-      keys = union(keys, Object.keys(pc))
-    })
-
-    const options = keys.map((key, index) => (
-      <option
-        key={index}
-        value={key}
-      >
-        {key}
-      </option>
-    ))
-
-    return (
-      <Input
-        type="select"
-        label='Feld mit eindeutiger ID in den Importdaten'
-        multiple
-        className='form-control controls'
-        style={style}
-        bsSize='small'
-        value={[idsImportIdField]}
-        onChange={(event) => onChangeImportId(event.target.value)}
-      >
-        {options}
-      </Input>
-    )
-  }
-})
+export default InputImportFields
