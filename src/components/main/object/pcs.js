f846ca8683e07c91b2ea5f61e89ee51b5f31c7ad
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 11:59:03 +0200

    comply to style

--- src/components/main/object/pcs.js
+++ src/components/main/object/pcs.js
@@ -3,36 +3,35 @@
 import React from 'react'
 import PropertyCollection from './pc.js'
 
-export default React.createClass({
-  displayName: 'PropertyCollections',
+const PropertyCollections = ({ object }) => {
+  const pcs = object.Eigenschaftensammlungen
+  let pcComponent = null
 
-  propTypes: {
-    object: React.PropTypes.object
-  },
-
-  render() {
-    const { object } = this.props
-    const pcs = object.Eigenschaftensammlungen
-    let pcComponent = null
-
-    if (pcs && pcs.length > 0) {
-      pcComponent = pcs.map((pc, index) => (
-        <PropertyCollection
-          key={index}
-          pcType="Datensammlung"
-          object={object}
-          propertyCollection={pc}
-        />
-      ))
-    }
-
-    return (
-      <div>
-        <h4>
-          Eigenschaften:
-        </h4>
-        {pcComponent}
-      </div>
-    )
+  if (pcs && pcs.length > 0) {
+    pcComponent = pcs.map((pc, index) => (
+      <PropertyCollection
+        key={index}
+        pcType="Datensammlung"
+        object={object}
+        propertyCollection={pc}
+      />
+    ))
   }
-})
+
+  return (
+    <div>
+      <h4>
+        Eigenschaften:
+      </h4>
+      {pcComponent}
+    </div>
+  )
+}
+
+PropertyCollections.displayName = 'PropertyCollections'
+
+PropertyCollections.propTypes = {
+  object: React.PropTypes.object
+}
+
+export default PropertyCollections
--- src/components/main/object/pcsOfSynonyms.js
+++ src/components/main/object/pcsOfSynonyms.js
@@ -3,36 +3,35 @@
 import React from 'react'
 import PropertyCollection from './pc.js'
 
-export default React.createClass({
-  displayName: 'PcsOfSynonyms',
+const PcsOfSynonyms = ({ pcsOfSynonyms, object }) => {
+  let pcsComponent = null
 
-  propTypes: {
-    object: React.PropTypes.object,
-    pcsOfSynonyms: React.PropTypes.array
-  },
-
-  render() {
-    const { pcsOfSynonyms, object } = this.props
-    let pcsComponent = null
-
-    if (pcsOfSynonyms.length > 0) {
-      pcsComponent = pcsOfSynonyms.map((pc, index) => (
-        <PropertyCollection
-          key={index}
-          pcType="Datensammlung"
-          object={object}
-          propertyCollection={pc}
-        />
-      ))
-    }
-
-    return (
-      <div>
-        <h4>
-          Eigenschaften von Synonymen:
-        </h4>
-        {pcsComponent}
-      </div>
-    )
+  if (pcsOfSynonyms.length > 0) {
+    pcsComponent = pcsOfSynonyms.map((pc, index) => (
+      <PropertyCollection
+        key={index}
+        pcType="Datensammlung"
+        object={object}
+        propertyCollection={pc}
+      />
+    ))
   }
-})
+
+  return (
+    <div>
+      <h4>
+        Eigenschaften von Synonymen:
+      </h4>
+      {pcsComponent}
+    </div>
+  )
+}
+
+PcsOfSynonyms.displayName = 'PcsOfSynonyms'
+
+PcsOfSynonyms.propTypes = {
+  object: React.PropTypes.object,
+  pcsOfSynonyms: React.PropTypes.array
+}
+
+export default PcsOfSynonyms
