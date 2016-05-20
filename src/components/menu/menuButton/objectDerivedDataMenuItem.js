be55e61b845a7fa78687d91c1aa626699b102e4e
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 13:12:01 +0200

    comply to style

--- src/components/menu/menuButton/objectDerivedDataMenuItem.js
+++ src/components/menu/menuButton/objectDerivedDataMenuItem.js
@@ -4,67 +4,61 @@ import React from 'react'
 import { MenuItem, OverlayTrigger, Popover, Glyphicon } from 'react-bootstrap'
 import rebuildRedundantData from '../../../modules/rebuildRedundantData.js'
 
-export default React.createClass({
-  displayName: 'ObjectDerivedDataMenuItem',
+const popover = () =>
+  <Popover
+    id="objectDerivedDataMenuItemPopover"
+    title="Was heisst das?"
+  >
+    <p>
+      Basierend auf den geladenen Objekten (Arten und Lebensräume)<br />
+      baut die Anwendung redundante Daten auf:
+    </p>
+    <ul>
+      <li>Taxonomie-Baum</li>
+      <li>Suchbegriffe</li>
+      <li>URL-Pfade für die Adresszeile des Browsers</li>
+    </ul>
+    <p>Diese Daten beschleunigen die Anwendung.</p>
+    <p>
+      Sie können jederzeit aus den Objekten neu aufgebaut werden.<br />
+      Normalerweise sollte das aber nicht nötig sein.
+    </p>
+    <p>Nutzen Sie diesen Befehl, wenn Sie Fehler in den erwähnten Daten finden.</p>
+  </Popover>
 
-  propTypes: {
-    offlineIndexes: React.PropTypes.bool,
-    onClickToggleOfflineIndexes: React.PropTypes.func
-  },
+const glyphiconStyle = {
+  paddingLeft: 5,
+  fontSize: 16,
+  top: 3
+}
 
-  rebuildRedundantData() {
-    rebuildRedundantData()
-  },
-
-  popover() {
-    return (
-      <Popover id="objectDerivedDataMenuItemPopover" title="Was heisst das?">
-        <p>
-          Basierend auf den geladenen Objekten (Arten und Lebensräume)<br />
-          baut die Anwendung redundante Daten auf:
-        </p>
-        <ul>
-          <li>Taxonomie-Baum</li>
-          <li>Suchbegriffe</li>
-          <li>URL-Pfade für die Adresszeile des Browsers</li>
-        </ul>
-        <p>Diese Daten beschleunigen die Anwendung.</p>
-        <p>
-          Sie können jederzeit aus den Objekten neu aufgebaut werden.<br />
-          Normalerweise sollte das aber nicht nötig sein.
-        </p>
-        <p>Nutzen Sie diesen Befehl, wenn Sie Fehler in den erwähnten Daten finden.</p>
-      </Popover>
-    )
-  },
-
-  render() {
-    const glyphiconStyle = {
-      paddingLeft: 5,
-      fontSize: 16,
-      top: 3
-    }
-    return (
-      <MenuItem
-        onClick={this.rebuildRedundantData}
+const ObjectDerivedDataMenuItem = () =>
+  <MenuItem
+    onClick={() => rebuildRedundantData()}
+  >
+    Redundante Daten neu aufbauen
+    <OverlayTrigger
+      trigger={['hover', 'focus']}
+      rootClose
+      placement="right"
+      overlay={popover()}
+    >
+      <span
+        className="withPopover"
       >
-        Redundante Daten neu aufbauen
-        <OverlayTrigger
-          trigger={['hover', 'focus']}
-          rootClose
-          placement="right"
-          overlay={this.popover()}
-        >
-          <span
-            className="withPopover"
-          >
-            <Glyphicon
-              glyph="info-sign"
-              style={glyphiconStyle}
-            />
-          </span>
-        </OverlayTrigger>
-      </MenuItem>
-    )
-  }
-})
+        <Glyphicon
+          glyph="info-sign"
+          style={glyphiconStyle}
+        />
+      </span>
+    </OverlayTrigger>
+  </MenuItem>
+
+ObjectDerivedDataMenuItem.displayName = 'ObjectDerivedDataMenuItem'
+
+ObjectDerivedDataMenuItem.propTypes = {
+  offlineIndexes: React.PropTypes.bool,
+  onClickToggleOfflineIndexes: React.PropTypes.func
+}
+
+export default ObjectDerivedDataMenuItem
--- src/components/menu/tree/loadingMessage.js
+++ src/components/menu/tree/loadingMessage.js
@@ -3,21 +3,27 @@
 import React from 'react'
 import { ProgressBar } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'LoadingMessage',
+const LoadingMessage = ({ groupLoadingObject }) => {
+  let { message, progressPercent } = groupLoadingObject
+  // Macromycetes shall appear as Pilze
+  message = message.replace('Macromycetes', 'Pilze')
 
-  propTypes: {
-    groupLoadingObject: React.PropTypes.object
-  },
-
-  render() {
-    const { groupLoadingObject } = this.props
-
-    let { message, progressPercent } = groupLoadingObject
-    // Macromycetes shall appear as Pilze
-    message = message.replace('Macromycetes', 'Pilze')
-
-    if (progressPercent || progressPercent === 0) return <ProgressBar bsStyle="success" now={progressPercent} label={message} />
-    return <p>{message}</p>
+  if (progressPercent || progressPercent === 0) {
+    return (
+      <ProgressBar
+        bsStyle="success"
+        now={progressPercent}
+        label={message}
+      />
+    )
   }
-})
+  return <p>{message}</p>
+}
+
+LoadingMessage.displayName = 'LoadingMessage'
+
+LoadingMessage.propTypes = {
+  groupLoadingObject: React.PropTypes.object
+}
+
+export default LoadingMessage
