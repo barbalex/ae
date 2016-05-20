41b05d5d11a7c9a020c5905932f804ab68a1daef
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 13:12:10 +0200

    comply to style

--- src/components/symbols/groupsLoading.js
+++ src/components/symbols/groupsLoading.js
@@ -3,27 +3,37 @@
 import React from 'react'
 import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'GroupsLoading',
+const tooltip = (
+  <Tooltip
+    id="groupsLoadingTooltip"
+    bsStyle="info"
+  >
+    Bitte Geduld: Die App kann zeitweise einfrieren!
+  </Tooltip>
+)
+const pStyle = {
+  color: 'red',
+  fontWeight: 500,
+  cursor: 'progress'
+}
 
-  propTypes: {
-    groupsLoadingObjects: React.PropTypes.array
-  },
+const GroupsLoading = ({ groupsLoadingObjects }) =>
+  <OverlayTrigger
+    placement="left"
+    overlay={tooltip}
+  >
+    <p
+      className="symbols text"
+      style={pStyle}
+    >
+      {groupsLoadingObjects.length > 1 ? 'Lade Gruppen...' : 'Lade Gruppe...'}
+    </p>
+  </OverlayTrigger>
 
-  render() {
-    const { groupsLoadingObjects } = this.props
-    const tooltip = <Tooltip id='groupsLoadingTooltip' bsStyle='info'>Bitte Geduld: Die App kann zeitweise einfrieren!</Tooltip>
-    const text = groupsLoadingObjects.length > 1 ? 'Lade Gruppen...' : 'Lade Gruppe...'
-    const pStyle = {
-      color: 'red',
-      fontWeight: 500,
-      cursor: 'progress'
-    }
+GroupsLoading.displayName = 'GroupsLoading'
 
-    return (
-      <OverlayTrigger placement='left' overlay={tooltip}>
-        <p className='symbols text' style={pStyle}>{text}</p>
-      </OverlayTrigger>
-    )
-  }
-})
+GroupsLoading.propTypes = {
+  groupsLoadingObjects: React.PropTypes.array
+}
+
+export default GroupsLoading
--- src/components/symbols/pcsQuerying.js
+++ src/components/symbols/pcsQuerying.js
@@ -3,21 +3,33 @@
 import React from 'react'
 import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'PcsQuerying',
+const tooltip = (
+  <Tooltip
+    id="pcsQueryingTooltip"
+    bsStyle="info"
+  >
+    Bitte Geduld: Die App kann zeitweise einfrieren!
+  </Tooltip>
+)
+const pStyle = {
+  color: 'red',
+  fontWeight: 500,
+  cursor: 'progress'
+}
 
-  render() {
-    const tooltip = <Tooltip id='pcsQueryingTooltip' bsStyle='info'>Bitte Geduld: Die App kann zeitweise einfrieren!</Tooltip>
-    const pStyle = {
-      color: 'red',
-      fontWeight: 500,
-      cursor: 'progress'
-    }
+const PcsQuerying = () =>
+  <OverlayTrigger
+    placement="left"
+    overlay={tooltip}
+  >
+    <p
+      className="symbols text"
+      style={pStyle}
+    >
+      Lade aktuelle Eigenschaftensammlungen...
+    </p>
+  </OverlayTrigger>
 
-    return (
-      <OverlayTrigger placement='left' overlay={tooltip}>
-        <p className='symbols text' style={pStyle}>Lade aktuelle Eigenschaftensammlungen...</p>
-      </OverlayTrigger>
-    )
-  }
-})
+PcsQuerying.displayName = 'PcsQuerying'
+
+export default PcsQuerying
