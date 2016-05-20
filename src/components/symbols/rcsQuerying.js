f132bf2f68be8204b31bf359eb310d07a866803f
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 13:12:14 +0200

    comply to style

--- src/components/symbols/rcsQuerying.js
+++ src/components/symbols/rcsQuerying.js
@@ -3,21 +3,33 @@
 import React from 'react'
 import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'RcsQuerying',
+const tooltip = (
+  <Tooltip
+    id="rcsQueryingTooltip"
+    bsStyle="info"
+  >
+    Bitte Geduld: Die App kann zeitweise einfrieren!
+  </Tooltip>
+)
+const pStyle = {
+  color: 'red',
+  fontWeight: 500,
+  cursor: 'progress'
+}
 
-  render() {
-    const tooltip = <Tooltip id='rcsQueryingTooltip' bsStyle='info'>Bitte Geduld: Die App kann zeitweise einfrieren!</Tooltip>
-    const pStyle = {
-      color: 'red',
-      fontWeight: 500,
-      cursor: 'progress'
-    }
+const RcsQuerying = () =>
+  <OverlayTrigger
+    placement="left"
+    overlay={tooltip}
+  >
+    <p
+      className="symbols text"
+      style={pStyle}
+    >
+      Lade aktuelle Beziehungssammlungen...
+    </p>
+  </OverlayTrigger>
 
-    return (
-      <OverlayTrigger placement='left' overlay={tooltip}>
-        <p className='symbols text' style={pStyle}>Lade aktuelle Beziehungssammlungen...</p>
-      </OverlayTrigger>
-    )
-  }
-})
+RcsQuerying.displayName = 'RcsQuerying'
+
+export default RcsQuerying
--- src/components/symbols/rebuildingRedundantData.js
+++ src/components/symbols/rebuildingRedundantData.js
@@ -3,27 +3,37 @@
 import React from 'react'
 import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 
-export default React.createClass({
+const tooltip = (
+  <Tooltip
+    id="rebuildingRedundantDataTooltip"
+    bsStyle="info"
+  >
+    Bitte Geduld: Die App kann zeitweise einfrieren!
+  </Tooltip>
+)
+const pStyle = {
+  color: 'red',
+  fontWeight: 500,
+  cursor: 'progress'
+}
 
-  displayName: 'RebuildingRedundantData',
+const RebuildingRedundantData = ({ rebuildingRedundantData }) =>
+  <OverlayTrigger
+    placement="left"
+    overlay={tooltip}
+  >
+    <p
+      className="symbols text"
+      style={pStyle}
+    >
+      {rebuildingRedundantData}
+    </p>
+  </OverlayTrigger>
 
-  propTypes: {
-    rebuildingRedundantData: React.PropTypes.string
-  },
+RebuildingRedundantData.displayName = 'RebuildingRedundantData'
 
-  render() {
-    const { rebuildingRedundantData } = this.props
-    const tooltip = <Tooltip id='rebuildingRedundantDataTooltip' bsStyle='info'>Bitte Geduld: Die App kann zeitweise einfrieren!</Tooltip>
-    const pStyle = {
-      color: 'red',
-      fontWeight: 500,
-      cursor: 'progress'
-    }
+RebuildingRedundantData.propTypes = {
+  rebuildingRedundantData: React.PropTypes.string
+}
 
-    return (
-      <OverlayTrigger placement='left' overlay={tooltip}>
-        <p className='symbols text' style={pStyle}>{rebuildingRedundantData}</p>
-      </OverlayTrigger>
-    )
-  }
-})
+export default RebuildingRedundantData
