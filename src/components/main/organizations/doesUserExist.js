9280f4b2c459c09f2c1ccc67deea1454ef01222f
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 13:11:38 +0200

    comply to style

--- src/components/main/organizations/doesUserExist.js
+++ src/components/main/organizations/doesUserExist.js
@@ -1,17 +1,15 @@
 'use strict'
 
-export default (username) => {
-  return new Promise((resolve, reject) => {
-    const url = `${window.location.protocol}//${window.location.hostname}:8000/doesuserexist/${username}`
-    fetch(url)
-      .then((result) => {
-        if (result && result.data) {
-          resolve(result.data)
-        } else {
-          resolve(null)
-        }
-      })
-      .catch((error) => reject((error))
-    )
-  })
-}
+export default (username) => new Promise((resolve, reject) => {
+  const url = `${window.location.protocol}//${window.location.hostname}:8000/doesuserexist/${username}`
+  fetch(url)
+    .then((result) => {
+      if (result && result.data) {
+        resolve(result.data)
+      } else {
+        resolve(null)
+      }
+    })
+    .catch((error) => reject((error))
+  )
+})
--- src/components/main/organizations/getRoleFromOrgField.js
+++ src/components/main/organizations/getRoleFromOrgField.js
@@ -2,9 +2,9 @@
 
 export default (organization, fieldName) => {
   const roleFromField = {
-    'esWriters': `${organization.Name} es`,
-    'lrWriters': `${organization.Name} lr`,
-    'orgAdmins': `${organization.Name} org`
+    esWriters: `${organization.Name} es`,
+    lrWriters: `${organization.Name} lr`,
+    orgAdmins: `${organization.Name} org`
   }
   return roleFromField[fieldName]
 }
