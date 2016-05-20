5268be0d31377dffdaaff1209d4055e604d66d0c
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 11:58:37 +0200

    comply to style

--- src/components/main/importRc/getObjectsFromFile.js
+++ src/components/main/importRc/getObjectsFromFile.js
@@ -3,38 +3,36 @@
 import { forEach } from 'lodash'
 import d3 from 'd3'
 
-export default (file) => {
-  return new Promise((resolve, reject) => {
-    const fileName = file.name
-    const fileType = fileName.split('.').pop()
-    let reader = new window.FileReader()
+export default (file) => new Promise((resolve, reject) => {
+  const fileName = file.name
+  const fileType = fileName.split('.').pop()
+  const reader = new window.FileReader()
 
-    if (fileType === 'csv') {
-      reader.onload = (onloadEvent) => {
-        const data = onloadEvent.target.result
-        const objects = d3.csv.parse(data)
-        // d3 adds missing fields as '' > remove them
-        objects.forEach((object, index) => {
-          forEach(object, (value, key) => {
-            if (value === '') delete objects[index][key]
-          })
+  if (fileType === 'csv') {
+    reader.onload = (onloadEvent) => {
+      const data = onloadEvent.target.result
+      const objects = d3.csv.parse(data)
+      // d3 adds missing fields as '' > remove them
+      objects.forEach((object, index) => {
+        forEach(object, (value, key) => {
+          if (value === '') delete objects[index][key]
         })
-        resolve(objects)
-      }
-      reader.onerror = (error) => reject('error reading file: ' + error)
-      reader.readAsText(file)
+      })
+      resolve(objects)
     }
-    if (fileType === 'xlsx') {
-      reader.onload = (onloadEvent) => {
-        const data = onloadEvent.target.result
-        const workbook = window.XLSX.read(data, {type: 'binary'})
-        const sheetName = workbook.SheetNames[0]
-        const worksheet = workbook.Sheets[sheetName]
-        const objects = window.XLSX.utils.sheet_to_json(worksheet)
-        resolve(objects)
-      }
-      reader.onerror = (error) => reject('error reading file: ' + error)
-      reader.readAsBinaryString(file)
+    reader.onerror = (error) => reject(`error reading file: ${error}`)
+    reader.readAsText(file)
+  }
+  if (fileType === 'xlsx') {
+    reader.onload = (onloadEvent) => {
+      const data = onloadEvent.target.result
+      const workbook = window.XLSX.read(data, { type: 'binary' })
+      const sheetName = workbook.SheetNames[0]
+      const worksheet = workbook.Sheets[sheetName]
+      const objects = window.XLSX.utils.sheet_to_json(worksheet)
+      resolve(objects)
     }
-  })
-}
+    reader.onerror = (error) => reject(`error reading file: ${error}`)
+    reader.readAsBinaryString(file)
+  }
+})
--- src/components/main/importRc/getSuccessTypeFromAnalysis.js
+++ src/components/main/importRc/getSuccessTypeFromAnalysis.js
@@ -1,6 +1,11 @@
 'use strict'
 
-export default ({ rcsToImport, idsNumberImportable, idsNotImportable, idsNotANumber }) => {
+export default ({
+  rcsToImport,
+  idsNumberImportable,
+  idsNotImportable,
+  idsNotANumber
+}) => {
   if (idsNotImportable.length > 0 || idsNotANumber.length > 0) return 'danger'
   if ((idsNumberImportable < rcsToImport.length)) return 'warning'
   return 'success'
