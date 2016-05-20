15b8e93cc59bdbe7ddaac48f857e88c81abddf08
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 10:45:06 +0200

    comply to style

--- src/components/main/importRc/panel2/tablePreview.js
+++ src/components/main/importRc/panel2/tablePreview.js
@@ -4,81 +4,81 @@ import React from 'react'
 import { union, without } from 'lodash'
 import { Table } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'TablePreview',
+const TablePreview = ({ rcsToImport }) => {
+  const legendStyle = { marginBottom: `${2}px`, paddingLeft: `${5}px` }
+  const tablePreviewStyle = { marginTop: `${10}px` }
+  let legend = ''
 
-  propTypes: {
-    rcsToImport: React.PropTypes.array
-  },
-
-  render() {
-    const { rcsToImport } = this.props
-    const legendStyle = { marginBottom: `${2}px`, paddingLeft: `${5}px` }
-    const tablePreviewStyle = { marginTop: `${10}px` }
-    let legend = ''
-
-    if (rcsToImport.length > 10) {
-      legend = `Vorschau der ersten 10 von ${rcsToImport.length} Datensätzen:`
-    } else if (rcsToImport.length > 1) {
-      legend = `Vorschau der ${rcsToImport.length} Datensätze:`
-    } else if (rcsToImport.length === 1) {
-      legend = 'Vorschau des einzigen Datensatzes:'
-    } else {
-      legend = 'Vorschau:'
-    }
-
-    // get a list of all keys
-    let keys = []
-    rcsToImport.forEach((pc) => {
-      keys = union(keys, Object.keys(pc))
-    })
-    // remove '_id' and 'rPartners' from keys
-    keys = without(keys, '_id', 'rPartners')
-
-    const thead = keys.map((key, index) => <th key={index}>{key}</th>)
-    const tbody = rcsToImport.map((pc, index) => {
-      // need only the first 10
-      if (index < 10) {
-        const rows = keys.map((key) => {
-          // return values for not existing fields!
-          // if not, table gets torn apart
-          const value = pc[key] || ''
-          return <td key={key}>{value}</td>
-        })
-        return (
-          <tr key={index}>
-            {rows}
-          </tr>
-        )
-      }
-      return null
-    })
-
-    return (
-      <div style={tablePreviewStyle}>
-        <p style={legendStyle}>{legend}</p>
-        {
-        /**
-         * surround table with panel to get rounded corners
-         * source: http://stackoverflow.com/questions/18729638/rounded-tables-in-twitter-bootstrap-3
-         */
-        }
-        <div
-          className="panel panel-default"
-          style={{ backgroundColor: '#fffff0' }}
-        >
-          <Table responsive bordered striped condensed hover>
-            <thead>
-              <tr>
-                {thead}
-              </tr>
-            </thead>
-            <tbody>
-              {tbody}
-            </tbody>
-          </Table>
-        </div>
-      </div>
-    )
+  if (rcsToImport.length > 10) {
+    legend = `Vorschau der ersten 10 von ${rcsToImport.length} Datensätzen:`
+  } else if (rcsToImport.length > 1) {
+    legend = `Vorschau der ${rcsToImport.length} Datensätze:`
+  } else if (rcsToImport.length === 1) {
+    legend = 'Vorschau des einzigen Datensatzes:'
+  } else {
+    legend = 'Vorschau:'
   }
-})
+
+  // get a list of all keys
+  let keys = []
+  rcsToImport.forEach((pc) => {
+    keys = union(keys, Object.keys(pc))
+  })
+  // remove '_id' and 'rPartners' from keys
+  keys = without(keys, '_id', 'rPartners')
+
+  const thead = keys.map((key, index) => <th key={index}>{key}</th>)
+  const tbody = rcsToImport.map((pc, index) => {
+    // need only the first 10
+    if (index < 10) {
+      const rows = keys.map((key) => {
+        // return values for not existing fields!
+        // if not, table gets torn apart
+        const value = pc[key] || ''
+        return <td key={key}>{value}</td>
+      })
+      return (
+        <tr key={index}>
+          {rows}
+        </tr>
+      )
+    }
+    return null
+  })
+
+  return (
+    <div style={tablePreviewStyle}>
+      <p style={legendStyle}>{legend}</p>
+      {
+      /**
+       * surround table with panel to get rounded corners
+       * source: http://stackoverflow.com/questions/18729638/rounded-tables-in-twitter-bootstrap-3
+       */
+      }
+      <div
+        className="panel panel-default"
+        style={{ backgroundColor: '#fffff0' }}
+      >
+        <Table responsive bordered striped condensed hover>
+          <thead>
+            <tr>
+              {thead}
+            </tr>
+          </thead>
+          <tbody>
+            {tbody}
+          </tbody>
+        </Table>
+      </div>
+    </div>
+  )
+}
+
+
+TablePreview.displayName = 'TablePreview'
+
+TablePreview.propTypes = {
+  rcsToImport: React.PropTypes.array
+}
+
+export default TablePreview
--- src/components/main/importRc/panel3/panel3.js
+++ src/components/main/importRc/panel3/panel3.js
@@ -5,73 +5,68 @@ import AlertIdsAnalysisResult from './alertIdsAnalysisResult.js'
 import InputImportFields from './inputImportFields.js'
 import InputAeId from './inputAeId.js'
 
-export default React.createClass({
-  displayName: 'Panel3',
+const Panel3 = ({
+  idsAeIdField,
+  idsNumberOfRecordsWithIdValue,
+  idsNumberImportable,
+  idsNotImportable,
+  idsNotANumber,
+  idsAnalysisComplete,
+  idsWithoutPartner,
+  rPartnerIdsToImport,
+  rPartnerIdsImportable,
+  idsImportIdField,
+  rcsToImport,
+  onChangeAeId,
+  onChangeImportId
+}) =>
+  <div>
+    {
+      rcsToImport.length > 0 &&
+      <InputImportFields
+        idsImportIdField={idsImportIdField}
+        rcsToImport={rcsToImport}
+        onChangeImportId={onChangeImportId}
+      />
+    }
+    <InputAeId
+      idsAeIdField={idsAeIdField}
+      onChangeAeId={onChangeAeId}
+    />
+    {
+      idsImportIdField && idsAeIdField &&
+      <AlertIdsAnalysisResult
+        idsImportIdField={idsImportIdField}
+        idsAeIdField={idsAeIdField}
+        rcsToImport={rcsToImport}
+        idsNumberOfRecordsWithIdValue={idsNumberOfRecordsWithIdValue}
+        idsNumberImportable={idsNumberImportable}
+        idsNotImportable={idsNotImportable}
+        idsAnalysisComplete={idsAnalysisComplete}
+        idsNotANumber={idsNotANumber}
+        idsWithoutPartner={idsWithoutPartner}
+        rPartnerIdsToImport={rPartnerIdsToImport}
+        rPartnerIdsImportable={rPartnerIdsImportable}
+      />
+    }
+  </div>
 
-  propTypes: {
-    rcsToImport: React.PropTypes.array,
-    idsImportIdField: React.PropTypes.string,
-    idsAeIdField: React.PropTypes.string,
-    idsAnalysisComplete: React.PropTypes.bool,
-    idsNumberOfRecordsWithIdValue: React.PropTypes.number,
-    idsNumberImportable: React.PropTypes.number,
-    idsNotImportable: React.PropTypes.array,
-    idsNotANumber: React.PropTypes.array,
-    idsWithoutPartner: React.PropTypes.array,
-    rPartnerIdsToImport: React.PropTypes.array,
-    rPartnerIdsImportable: React.PropTypes.array,
-    onChangeAeId: React.PropTypes.func,
-    onChangeImportId: React.PropTypes.func
-  },
+Panel3.displayName = 'Panel3'
 
-  render() {
-    const {
-      idsAeIdField,
-      idsNumberOfRecordsWithIdValue,
-      idsNumberImportable,
-      idsNotImportable,
-      idsNotANumber,
-      idsAnalysisComplete,
-      idsWithoutPartner,
-      rPartnerIdsToImport,
-      rPartnerIdsImportable,
-      idsImportIdField,
-      rcsToImport,
-      onChangeAeId,
-      onChangeImportId
-    } = this.props
+Panel3.propTypes = {
+  rcsToImport: React.PropTypes.array,
+  idsImportIdField: React.PropTypes.string,
+  idsAeIdField: React.PropTypes.string,
+  idsAnalysisComplete: React.PropTypes.bool,
+  idsNumberOfRecordsWithIdValue: React.PropTypes.number,
+  idsNumberImportable: React.PropTypes.number,
+  idsNotImportable: React.PropTypes.array,
+  idsNotANumber: React.PropTypes.array,
+  idsWithoutPartner: React.PropTypes.array,
+  rPartnerIdsToImport: React.PropTypes.array,
+  rPartnerIdsImportable: React.PropTypes.array,
+  onChangeAeId: React.PropTypes.func,
+  onChangeImportId: React.PropTypes.func
+}
 
-    return (
-      <div>
-        {
-          rcsToImport.length > 0 &&
-          <InputImportFields
-            idsImportIdField={idsImportIdField}
-            rcsToImport={rcsToImport}
-            onChangeImportId={onChangeImportId}
-          />
-        }
-        <InputAeId
-          idsAeIdField={idsAeIdField}
-          onChangeAeId={onChangeAeId}
-        />
-        {
-          idsImportIdField && idsAeIdField &&
-          <AlertIdsAnalysisResult
-            idsImportIdField={idsImportIdField}
-            idsAeIdField={idsAeIdField}
-            rcsToImport={rcsToImport}
-            idsNumberOfRecordsWithIdValue={idsNumberOfRecordsWithIdValue}
-            idsNumberImportable={idsNumberImportable}
-            idsNotImportable={idsNotImportable}
-            idsAnalysisComplete={idsAnalysisComplete}
-            idsNotANumber={idsNotANumber}
-            idsWithoutPartner={idsWithoutPartner}
-            rPartnerIdsToImport={rPartnerIdsToImport}
-            rPartnerIdsImportable={rPartnerIdsImportable}
-          />
-        }
-      </div>
-    )
-  }
-})
+export default Panel3
