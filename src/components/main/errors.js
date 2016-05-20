2e9b3fd7e0f2cb7d588ecc1e4d79bb530e48b187
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 13:11:29 +0200

    comply to style

--- src/components/main/errors.js
+++ src/components/main/errors.js
@@ -14,65 +14,59 @@ import app from 'ampersand-app'
 import React from 'react'
 import { Overlay, Glyphicon } from 'react-bootstrap'
 
-export default React.createClass({
-  displayName: 'Errors',
-
-  propTypes: {
-    errors: React.PropTypes.array
-  },
-
-  onClickRemove() {
-    app.Actions.showError()
-  },
-
-  render() {
-    const { errors } = this.props
-    const show = errors.length > 0
-    const errorMessages = errors.map((error, index) => (
-      <div
-        className='errorContainer'
-        key={index}>
-        <div
-          className='error'>
-          {
-            error.title &&
-            <p>{error.title}</p>
-          }
-          <p>
-            <em>
-              {error.msg}
-            </em>
-          </p>
-        </div>
-        {
-          index + 1 < errors.length &&
-          <hr/>
-        }
-      </div>
-    ))
-    const glyphStyle = {
-      position: 'absolute',
-      top: 3 + 'px',
-      right: 3 + 'px',
-      fontSize: 18 + 'px',
-      cursor: 'pointer'
+const errorMessages = (errors) => errors.map((error, index) => (
+  <div
+    className="errorContainer"
+    key={index}
+  >
+    <div className="error">
+      {
+        error.title &&
+        <p>
+          {error.title}
+        </p>
+      }
+      <p>
+        <em>
+          {error.msg}
+        </em>
+      </p>
+    </div>
+    {
+      index + 1 < errors.length &&
+      <hr />
     }
+  </div>
+))
 
-    return (
-      <Overlay
-        show={show}
-        container={this}
-        placement='top'
-      >
-        <div
-          id='errors'>
-          <Glyphicon
-            glyph='remove-circle'
-            style={glyphStyle}
-            onClick={this.onClickRemove} />
-          {errorMessages}
-        </div>
-      </Overlay>
-    )
-  }
-})
+const glyphStyle = {
+  position: 'absolute',
+  top: `${3}px`,
+  right: `${3}px`,
+  fontSize: `${18}px`,
+  cursor: 'pointer'
+}
+
+const Errors = ({ errors }) =>
+  <Overlay
+    show={errors.length > 0}
+    container={this}
+    placement="top"
+  >
+    <div id="errors">
+      <Glyphicon
+        glyph="remove-circle"
+        style={glyphStyle}
+        onClick={() => app.Actions.showError()}
+      />
+      {errorMessages(errors)}
+    </div>
+  </Overlay>
+
+Errors.displays = 'Errors'
+
+Errors.propTypes = {
+  errors: React.PropTypes.array
+}
+
+export default Errors
--- src/components/main/fourOhFour.js
+++ src/components/main/fourOhFour.js
@@ -2,14 +2,11 @@
 
 import React from 'react'
 
-export default React.createClass({
-  displayName: 'fourOhFourPage',
+const FourOhFourPage = () =>
+  <fieldset id="main">
+    <h1>Sorry: Für diese URL existiert keine Seite</h1>
+  </fieldset>
 
-  render() {
-    return (
-      <fieldset id='main'>
-        <h1>Sorry: Für diese URL existiert keine Seite</h1>
-      </fieldset>
-    )
-  }
-})
+FourOhFourPage.displayName = 'FourOhFourPage'
+
+export default FourOhFourPage
