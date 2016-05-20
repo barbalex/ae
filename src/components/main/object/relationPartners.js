e695f7c695fcd8d43f04f744a96a049c73c156f9
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 11:59:19 +0200

    comply to style

--- src/components/main/object/relationPartners.js
+++ src/components/main/object/relationPartners.js
@@ -7,42 +7,41 @@
 import React from 'react'
 import TextLink from './textLink.js'
 
-export default React.createClass({
-  displayName: 'RelationPartners',
+const RelationPartners = ({ relation }) => {
+  let relationPartners = []
 
-  propTypes: {
-    relation: React.PropTypes.object
-  },
+  if (relation.Beziehungspartner) {
+    relation.Beziehungspartner.forEach((bezPartner, index) => {
+      // label field with Rolle if it exists
+      let label = bezPartner.Rolle ? bezPartner.Rolle : 'Beziehungspartner'
+      // give only the first bezPartner a label
+      label = index > 0 ? null : label
+      const value = `${bezPartner.Gruppe}: ${bezPartner.Taxonomie ? `${bezPartner.Taxonomie} > ` : ''}${bezPartner.Name}`
 
-  render() {
-    const { relation } = this.props
-    let relationPartners = []
-
-    if (relation.Beziehungspartner) {
-      relation.Beziehungspartner.forEach((bezPartner, index) => {
-        // label field with Rolle if it exists
-        let label = bezPartner.Rolle ? bezPartner.Rolle : 'Beziehungspartner'
-        // give only the first bezPartner a label
-        label = index > 0 ? null : label
-        const value = `${bezPartner.Gruppe}: ${bezPartner.Taxonomie ? `${bezPartner.Taxonomie} > ` : ''}${bezPartner.Name}`
-
-        const textLink = (
-          <TextLink
-            key={bezPartner.GUID}
-            label={label}
-            value={value}
-            gruppe={bezPartner.Gruppe}
-            guid={bezPartner.GUID}
-          />
-        )
-        relationPartners.push(textLink)
-      })
-    }
-
-    return (
-      <div>
-        {relationPartners}
-      </div>
-    )
+      const textLink = (
+        <TextLink
+          key={bezPartner.GUID}
+          label={label}
+          value={value}
+          gruppe={bezPartner.Gruppe}
+          guid={bezPartner.GUID}
+        />
+      )
+      relationPartners.push(textLink)
+    })
   }
-})
+
+  return (
+    <div>
+      {relationPartners}
+    </div>
+  )
+}
+
+RelationPartners.displayName = 'RelationPartners'
+
+RelationPartners.propTypes = {
+  relation: React.PropTypes.object
+}
+
+export default RelationPartners
--- src/components/main/object/taxonomy.js
+++ src/components/main/object/taxonomy.js
@@ -3,40 +3,47 @@
 import React from 'react'
 import PropertyCollection from './pc.js'
 
-export default React.createClass({
-  displayName: 'Taxonomy',
+const Taxonomy = ({
+  object,
+  onSaveObjectField,
+  userRoles,
+  editObjects,
+  toggleEditObjects,
+  addNewObject,
+  removeObject
+}) => {
+  const standardtaxonomie = object.Taxonomien.find((taxonomy) => taxonomy.Standardtaxonomie)
 
-  propTypes: {
-    object: React.PropTypes.object,
-    onSaveObjectField: React.PropTypes.func,
-    userRoles: React.PropTypes.array,
-    editObjects: React.PropTypes.bool,
-    toggleEditObjects: React.PropTypes.func,
-    addNewObject: React.PropTypes.func,
-    removeObject: React.PropTypes.func
-  },
+  return (
+    <div>
+      <h4>
+        Taxonomie:
+      </h4>
+      <PropertyCollection
+        pcType="Taxonomie"
+        object={object}
+        onSaveObjectField={onSaveObjectField}
+        propertyCollection={standardtaxonomie}
+        userRoles={userRoles}
+        editObjects={editObjects}
+        toggleEditObjects={toggleEditObjects}
+        addNewObject={addNewObject}
+        removeObject={removeObject}
+      />
+    </div>
+  )
+}
 
-  render() {
-    const { object, onSaveObjectField, userRoles, editObjects, toggleEditObjects, addNewObject, removeObject } = this.props
-    const standardtaxonomie = object.Taxonomien.find((taxonomy) => taxonomy.Standardtaxonomie)
+Taxonomy.displayName = 'Taxonomy'
 
-    return (
-      <div>
-        <h4>
-          Taxonomie:
-        </h4>
-        <PropertyCollection
-          pcType="Taxonomie"
-          object={object}
-          onSaveObjectField={onSaveObjectField}
-          propertyCollection={standardtaxonomie}
-          userRoles={userRoles}
-          editObjects={editObjects}
-          toggleEditObjects={toggleEditObjects}
-          addNewObject={addNewObject}
-          removeObject={removeObject}
-        />
-      </div>
-    )
-  }
-})
+Taxonomy.propTypes = {
+  object: React.PropTypes.object,
+  onSaveObjectField: React.PropTypes.func,
+  userRoles: React.PropTypes.array,
+  editObjects: React.PropTypes.bool,
+  toggleEditObjects: React.PropTypes.func,
+  addNewObject: React.PropTypes.func,
+  removeObject: React.PropTypes.func
+}
+
+export default Taxonomy
