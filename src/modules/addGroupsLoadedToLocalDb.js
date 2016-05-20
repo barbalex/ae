5fede9a4dc5125db5e977fcc8774512b2df4c471
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-20 15:01:30 +0200

    comply to style

--- src/modules/addGroupsLoadedToLocalDb.js
+++ src/modules/addGroupsLoadedToLocalDb.js
@@ -9,18 +9,17 @@
 import app from 'ampersand-app'
 import { union } from 'lodash'
 
-export default (groups) => {
-  return new Promise((resolve, reject) => {
-    let groupsLoaded = []
-    app.localDb.get('_local/groupsLoaded')
-      .then((doc) => {
-        // make shure a group is not included more than once
-        groupsLoaded = union(doc.groupsLoaded, groups)
-        doc.groupsLoaded = groupsLoaded
-        return app.localDb.put(doc)
-      })
-      .then(() => resolve(groupsLoaded))
-      .catch((error) => reject('addGroupsLoadedToLocalDb: error getting or putting groups "' + groups + '" from localDb:', error)
+export default (groups) => new Promise((resolve, reject) => {
+  let groupsLoaded = []
+  app.localDb.get('_local/groupsLoaded')
+    .then((doc) => {
+      // make shure a group is not included more than once
+      groupsLoaded = union(doc.groupsLoaded, groups)
+      doc.groupsLoaded = groupsLoaded
+      return app.localDb.put(doc)
+    })
+    .then(() => resolve(groupsLoaded))
+    .catch((error) =>
+      reject(`addGroupsLoadedToLocalDb: error getting or putting groups "${groups}" from localDb:`, error)
     )
-  })
-}
+})
--- src/modules/addPathsFromItemsToLocalDb.js
+++ src/modules/addPathsFromItemsToLocalDb.js
@@ -9,38 +9,36 @@
 import app from 'ampersand-app'
 import getPathFromObject from './getPathFromObject.js'
 
-export default (objects) => {
-  return new Promise((resolve, reject) => {
-    // build paths of passed in objects (usually: objects of a group)
-    const pathsOfGruppe = {}
-    objects.forEach((object) => {
-      // get hierarchy from the Hierarchie field
-      // default value (in case there is none) is []
-      const path = getPathFromObject(object)
-      // if path is [] make sure no path is added
-      if (path.length > 0) {
-        const url = path.join('/')
-        pathsOfGruppe[url] = object._id
-      }
-    })
-
-    // console.log('addPathsFromItemsToLocalDb.js, paths of gruppe ' + objects[0].Gruppe, pathsOfGruppe)
-
-    // combine these paths with those already in pathDb
-    app.localDb.get('_local/paths', (error, doc) => {
-      if (error) {
-        if (error.status === 404) {
-          // leave paths as it is
-          console.log('error getting _local/paths', error)
-        } else {
-          reject('addPathsFromItemsToLocalDb.js: error getting paths from localDb:', error)
-        }
-      }
-      doc.paths = Object.assign(doc.paths, pathsOfGruppe)
-      app.localDb.put(doc)
-        .then(() => resolve(doc.paths))
-        .catch((error) => reject('addPathsFromItemsToLocalDb.js: error writing paths to localDb:', error)
-      )
-    })
+export default (objects) => new Promise((resolve, reject) => {
+  // build paths of passed in objects (usually: objects of a group)
+  const pathsOfGruppe = {}
+  objects.forEach((object) => {
+    // get hierarchy from the Hierarchie field
+    // default value (in case there is none) is []
+    const path = getPathFromObject(object)
+    // if path is [] make sure no path is added
+    if (path.length > 0) {
+      const url = path.join('/')
+      pathsOfGruppe[url] = object._id
+    }
   })
-}
+
+  // console.log('addPathsFromItemsToLocalDb.js, paths of gruppe ' + objects[0].Gruppe, pathsOfGruppe)
+
+  // combine these paths with those already in pathDb
+  app.localDb.get('_local/paths', (error, doc) => {
+    if (error) {
+      if (error.status === 404) {
+        // leave paths as it is
+        console.log('error getting _local/paths', error)
+      } else {
+        reject('addPathsFromItemsToLocalDb.js: error getting paths from localDb:', error)
+      }
+    }
+    doc.paths = Object.assign(doc.paths, pathsOfGruppe)
+    app.localDb.put(doc)
+      .then(() => resolve(doc.paths))
+      .catch((err) => reject('addPathsFromItemsToLocalDb.js: error writing paths to localDb:', err)
+    )
+  })
+})
