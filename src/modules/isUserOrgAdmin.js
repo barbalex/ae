e1e18d5cc37e4de8f78a31a9f7e1e8a39998da77
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-26 17:44:42 +0200

    optimize code

--- src/modules/isUserOrgAdmin.js
+++ src/modules/isUserOrgAdmin.js
@@ -4,8 +4,12 @@ import { isArray, isString } from 'lodash'
 
 export default (roles, org) => {
   // test input
-  if (!roles || !isArray(roles)) return false
-  if (!org || !isString(org)) return false
+  if (!roles || !isArray(roles)) {
+    return false
+  }
+  if (!org || !isString(org)) {
+    return false
+  }
   // check if user is admin for org
   const orgAdminRole = `${org} org`
   return roles.includes(orgAdminRole)
--- src/modules/isUserOrgAdminAnywhere.js
+++ src/modules/isUserOrgAdminAnywhere.js
@@ -4,8 +4,12 @@ import { isArray } from 'lodash'
 
 export default (roles) => {
   // test input
-  if (!roles || !isArray(roles)) return false
+  if (!roles || !isArray(roles)) {
+    return false
+  }
   // check if user is admin for any org
-  const adminRoles = roles.map((role) => role.endsWith(' org'))
+  const adminRoles = roles.map((role) =>
+    role.endsWith(' org')
+  )
   return adminRoles.length > 0
 }
