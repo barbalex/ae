e9fa55654e1e087070d0acfc0d64ff61467086ab
Alexander Gabriel <alex.barbalex@gmail.com>
2016-05-26 11:08:49 +0200

    optimize code

--- src/components/main/export/panel4/isFilterFulfilledForBeziehungspartner.js
+++ src/components/main/export/panel4/isFilterFulfilledForBeziehungspartner.js
@@ -6,7 +6,10 @@ export default (rPartners, filterValue, co) => {
   const rPartnersToReturn = []
   if (rPartners && rPartners.length > 0) {
     rPartners.forEach((rPartner) => {
-      if (isFilterFulfilled(rPartner.Name, filterValue, co) || isFilterFulfilled(rPartner.GUID, filterValue, co)) {
+      if (
+        isFilterFulfilled(rPartner.Name, filterValue, co) ||
+        isFilterFulfilled(rPartner.GUID, filterValue, co)
+      ) {
         rPartnersToReturn.push(rPartner)
       }
     })
