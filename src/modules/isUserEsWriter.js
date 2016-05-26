3459d7f009713405603c18c794968b3569058fdc
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-26 17:44:37 +0200

    optimize code

--- src/modules/isUserEsWriter.js
+++ src/modules/isUserEsWriter.js
@@ -4,8 +4,12 @@ import { isArray, isString } from 'lodash'
 
 export default (roles, org) => {
   // test input
-  if (!roles || !isArray(roles)) return false
-  if (!org || !isString(org)) return false
+  if (!roles || !isArray(roles)) {
+    return false
+  }
+  if (!org || !isString(org)) {
+    return false
+  }
   // check if user is esWriter for org
   const esWriterRole = `${org} es`
   return roles.includes(esWriterRole)
