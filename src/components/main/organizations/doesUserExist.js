0e3f79b84754166b1aaf8693a6059f521be8573d
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-26 17:44:33 +0200

    optimize code

--- src/components/main/organizations/doesUserExist.js
+++ src/components/main/organizations/doesUserExist.js
@@ -1,15 +1,15 @@
 'use strict'
 
-export default (username) => new Promise((resolve, reject) => {
-  const url = `${window.location.protocol}//${window.location.hostname}:8000/doesuserexist/${username}`
-  fetch(url)
-    .then((result) => {
-      if (result && result.data) {
-        resolve(result.data)
-      } else {
-        resolve(null)
-      }
-    })
-    .catch((error) => reject((error))
+export default (username) =>
+  new Promise((resolve, reject) =>
+    fetch(`${window.location.protocol}//${window.location.hostname}:8000/doesuserexist/${username}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data) {
+          resolve(data)
+        } else {
+          resolve(null)
+        }
+      })
+      .catch((error) => reject(error))
   )
-})
--- src/components/main/organizations/getRolesOfUser.js
+++ src/components/main/organizations/getRolesOfUser.js
@@ -1,9 +1,9 @@
 'use strict'
 
-export default (username) => new Promise((resolve, reject) => {
-  const url = `${window.location.protocol}//${window.location.hostname}:8000/userroles/${username}`
-  fetch(url)
-    .then((result) => resolve(result.data))
-    .catch((error) => reject((error))
+export default (username) =>
+  new Promise((resolve, reject) =>
+    fetch(`${window.location.protocol}//${window.location.hostname}:8000/userroles/${username}`)
+      .then(response => response.json())
+      .then((data) => resolve(data))
+      .catch((error) => reject(error))
   )
-})
