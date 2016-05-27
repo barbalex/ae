cd240a804b8c1437882c2ceb821f25aa4b5de066
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-27 15:34:53 +0200

    Revert "optimize code"
    This reverts commit 40eaa50fe660180c69e82fb59469f5fbd3072991.

--- src/components/main/importRc/panel2/wellAnforderungenAnCsv.js
+++ src/components/main/importRc/panel2/wellAnforderungenAnCsv.js
@@ -23,11 +23,12 @@ export default React.createClass({
 
   render() {
     const { visible } = this.state
+    /**
+     * TODO: Braucht es jetzt Hochzeichen um Feldinhalt?
+     */
     return (
       <Well bsSize="small">
-        <b>
-          Zusätzliche Anforderungen an csv-Dateien
-        </b>
+        <b>Zusätzliche Anforderungen an csv-Dateien</b>
         &nbsp;
         <a
           href="#"
