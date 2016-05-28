df815e954759e714c8e26a9152f117db925aef1a
Alexander Gabriel <alex.barbalex@gmail.com>
2016-05-28 19:46:24 +0200

    optimize code

--- src/stores/user.js
+++ src/stores/user.js
@@ -4,52 +4,49 @@ import app from 'ampersand-app'
 import Reflux from 'reflux'
 import refreshUserRoles from '../modules/refreshUserRoles.js'
 
-export default (Actions) => {
-  const userStore = Reflux.createStore({
-    /*
-     * contains email and roles of logged in user
-     * well, it is saved in pouch as local doc _local/login
-     * and contains "logIn" bool which states if user needs to log in
-     * app.js sets default _local/login if not exists on app start
-     */
-    listenables: Actions,
+export default (Actions) => Reflux.createStore({
+  /*
+   * contains email and roles of logged in user
+   * well, it is saved in pouch as local doc _local/login
+   * and contains "logIn" bool which states if user needs to log in
+   * app.js sets default _local/login if not exists on app start
+   */
+  listenables: Actions,
 
-    getLogin() {
-      return new Promise((resolve, reject) => {
-        app.localDb.get('_local/login')
-          .then((doc) => {
-            refreshUserRoles(doc.email)
-            resolve(doc)
-          })
-          .catch((error) =>
-            reject(`userStore: error getting login from localDb: ${error}`)
-          )
-      })
-    },
-
-    onLogin({ logIn, email, roles }) {
+  getLogin() {
+    return new Promise((resolve, reject) => {
       app.localDb.get('_local/login')
         .then((doc) => {
-          doc.logIn = logIn
-          doc.email = email || undefined
-          doc.roles = roles || []
-          this.trigger({ logIn, email, roles })
-          /*
-           * need to requery organizations if they have been loaded
-           * because isUserAdmin needs to be updated
-           */
-          if (email && app.organizationsStore.organizations.length > 0) {
-            app.Actions.getOrganizations(email)
-          }
-          return app.localDb.put(doc)
+          refreshUserRoles(doc.email)
+          resolve(doc)
         })
         .catch((error) =>
-          app.Actions.showError({
-            title: 'userStore: error logging in:',
-            msg: error
-          })
+          reject(`userStore: error getting login from localDb: ${error}`)
         )
-    }
-  })
-  return userStore
-}
+    })
+  },
+
+  onLogin({ logIn, email, roles }) {
+    app.localDb.get('_local/login')
+      .then((doc) => {
+        doc.logIn = logIn
+        doc.email = email || undefined
+        doc.roles = roles || []
+        this.trigger({ logIn, email, roles })
+        /*
+         * need to requery organizations if they have been loaded
+         * because isUserAdmin needs to be updated
+         */
+        if (email && app.organizationsStore.organizations.length > 0) {
+          app.Actions.getOrganizations(email)
+        }
+        return app.localDb.put(doc)
+      })
+      .catch((error) =>
+        app.Actions.showError({
+          title: 'userStore: error logging in:',
+          msg: error
+        })
+      )
+  }
+})
--- src/stores/users.js
+++ src/stores/users.js
@@ -4,35 +4,32 @@ import app from 'ampersand-app'
 import Reflux from 'reflux'
 import { map } from 'lodash'
 
-export default (Actions) => {
-  const usersStore = Reflux.createStore({
-    /**
-     * used to cache users names
-     */
-    listenables: Actions,
+export default (Actions) => Reflux.createStore({
+  /**
+   * used to cache users names
+   */
+  listenables: Actions,
 
-    userNames: [],
+  userNames: [],
 
-    onGetUsers() {
-      if (this.userNames.length > 0) {
-        this.trigger(this.userNames)
-      }
-      app.remoteUsersDb.allDocs({ include_docs: true })
-        .then((result) => {
-          const users = result.rows.map((row) =>
-            row.doc
-          )
-          const userNames = map(users, 'name')
-          this.userNames = userNames
-          this.trigger(this.userNames)
-        })
-        .catch((error) =>
-          app.Actions.showError({
-            title: 'error fetching organizations from remoteDb:',
-            msg: error
-          })
-        )
+  onGetUsers() {
+    if (this.userNames.length > 0) {
+      this.trigger(this.userNames)
     }
-  })
-  return usersStore
-}
+    app.remoteUsersDb.allDocs({ include_docs: true })
+      .then((result) => {
+        const users = result.rows.map((row) =>
+          row.doc
+        )
+        const userNames = map(users, 'name')
+        this.userNames = userNames
+        this.trigger(this.userNames)
+      })
+      .catch((error) =>
+        app.Actions.showError({
+          title: 'error fetching organizations from remoteDb:',
+          msg: error
+        })
+      )
+  }
+})
