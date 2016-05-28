3acd2d2327a67cf2dc35676bba3e189c2a540c45
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-28 21:27:22 +0200

    style with aphrodite

--- src/components/menu/groups/groups.js
+++ src/components/menu/groups/groups.js
@@ -2,10 +2,35 @@
 
 import React from 'react'
 import { difference } from 'lodash'
+import { StyleSheet, css } from 'aphrodite'
 import GroupCheckbox from './groupCheckbox.js'
 import getGruppen from '../../../modules/gruppen.js'
 
 const gruppen = getGruppen()
+
+const styles = StyleSheet.create({
+  groupsDiv: {
+    float: 'left',
+    clear: 'both',
+    width: '100%',
+    padding: '5px 10px',
+    border: '1px solid transparent',
+    borderRadius: '3px',
+    backgroundColor: 'rgb(255, 255, 255)',
+    borderColor: 'rgb(204, 204, 204)',
+    color: '#333',
+    margin: 0,
+    marginBottom: 5
+  },
+  labelDiv: {
+    height: 12
+  },
+  groupCheckboxesDiv: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    height: 30
+  }
+})
 
 const Groups = ({ groupsLoadedOrLoading }) => {
   const groupsNotLoaded = difference(gruppen, groupsLoadedOrLoading)
@@ -18,26 +43,13 @@ const Groups = ({ groupsLoadedOrLoading }) => {
 
   return (
     <div
-      style={{
-        padding: '5px 10px',
-        border: '1px solid transparent',
-        borderRadius: '3px',
-        backgroundColor: 'rgb(255, 255, 255)',
-        borderColor: 'rgb(204, 204, 204)',
-        color: '#333',
-        margin: 0,
-        marginBottom: 5
-      }}
+      className={css(styles.groupsDiv)}
     >
-      <div style={{ height: 12 }}>
+      <div className={css(styles.labelDiv)}>
         Gruppen laden:
       </div>
       <div
-        style={{
-          display: 'flex',
-          justifyContent: 'space-between',
-          height: 30
-        }}
+        className={css(styles.groupCheckboxesDiv)}
       >
         {groupCheckboxes}
       </div>
