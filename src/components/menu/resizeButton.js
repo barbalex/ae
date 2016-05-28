71c477f632bb3c8031b9acdafcc1d3b1665d5335
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-28 21:26:53 +0200

    style with aphrodite

--- src/components/menu/resizeButton.js
+++ src/components/menu/resizeButton.js
@@ -3,9 +3,19 @@
 import React from 'react'
 import hasClass from 'amp-has-class'
 import toggleClass from 'amp-toggle-class'
+import { StyleSheet, css } from 'aphrodite'
 import { Tooltip, OverlayTrigger, Glyphicon, Button } from 'react-bootstrap'
 
 const bodyElement = document.body
+
+const styles = StyleSheet.create({
+  rootDiv: {
+    float: 'left',
+    clear: 'both',
+    width: '100%',
+    marginBottom: 5
+  }
+})
 
 export default React.createClass({
   displayName: 'ResizeButton',
@@ -17,29 +27,31 @@ export default React.createClass({
 
   render() {
     return (
-      <OverlayTrigger
-        placement="left"
-        overlay={
-          <Tooltip
-            id="btnResizeTooltip"
-          >
-            {
-              hasClass(bodyElement, 'force-mobile') ?
-              'in zwei Spalten anzeigen' :
-              'ganze Breite nutzen'
-            }
-          </Tooltip>
-        }
-      >
-        <Button
-          id="btnResize"
-          className="pull-right"
-          bsSize="small"
-          onClick={this.resize}
+      <div className={css(styles.rootDiv)}>
+        <OverlayTrigger
+          placement="left"
+          overlay={
+            <Tooltip
+              id="btnResizeTooltip"
+            >
+              {
+                hasClass(bodyElement, 'force-mobile') ?
+                'in zwei Spalten anzeigen' :
+                'ganze Breite nutzen'
+              }
+            </Tooltip>
+          }
         >
-          <Glyphicon glyph="resize-horizontal" />
-        </Button>
-      </OverlayTrigger>
+          <Button
+            id="btnResize"
+            className="pull-right"
+            bsSize="small"
+            onClick={this.resize}
+          >
+            <Glyphicon glyph="resize-horizontal" />
+          </Button>
+        </OverlayTrigger>
+      </div>
     )
   }
 })
