b2848fc67ebf564d3b9d6c7260ce86a68954f194
Alexander Gabriel <alex.barbalex@gmail.com>
2016-05-28 19:45:49 +0200

    optimize code

--- src/stores/filterOptions.js
+++ src/stores/filterOptions.js
@@ -5,67 +5,64 @@ import Reflux from 'reflux'
 import buildFilterOptions from '../modules/buildFilterOptions.js'
 import buildFilterOptionsFromObject from '../modules/buildFilterOptionsFromObject.js'
 
-export default (Actions) => {
-  const filterOptionsStore = Reflux.createStore({
-    /*
-     * simple store that keeps an array of filter options
-     * because creating them uses a lot of cpu
-     * well, they are kept in localDb in _local/filterOptions
-    */
-    listenables: Actions,
+export default (Actions) => Reflux.createStore({
+  /*
+   * simple store that keeps an array of filter options
+   * because creating them uses a lot of cpu
+   * well, they are kept in localDb in _local/filterOptions
+  */
+  listenables: Actions,
 
-    getOptions() {
-      return new Promise((resolve, reject) => {
-        app.localDb.get('_local/filterOptions')
-          .then((doc) => resolve(doc.filterOptions))
-          .catch((error) =>
-            reject('filterOptionsStore: error fetching filterOptions from localDb:', error)
-          )
-      })
-    },
-
-    onLoadFilterOptions(newItemsPassed) {
-      this.trigger({
-        filterOptions: null,
-        loading: true
-      })
-      // get existing filterOptions
-      this.getOptions()
-        .then((optionsFromPouch) => {
-          let filterOptions = optionsFromPouch
-          if (newItemsPassed) filterOptions = filterOptions.concat(buildFilterOptions(newItemsPassed))
-          const loading = false
-          this.trigger({ filterOptions, loading })
-        })
-        .catch((error) =>
-          app.Actions.showError({
-            title: 'filterOptionsStore, onLoadFilterOptions: error preparing trigger:',
-            msg: error
-          })
-        )
-    },
-
-    onChangeFilterOptionsForObject(object) {
-      const option = buildFilterOptionsFromObject(object)
-      let options = null
+  getOptions() {
+    return new Promise((resolve, reject) => {
       app.localDb.get('_local/filterOptions')
-        .then((doc) => {
-          // replace option with new
-          doc.filterOptions = doc.filterOptions.filter((op) =>
-            op.value !== object._id
-          )
-          doc.filterOptions.push(option)
-          options = doc.filterOptions
-          return app.localDb.put(doc)
-        })
-        .then(() => this.trigger({ options, loading: false }))
+        .then((doc) => resolve(doc.filterOptions))
         .catch((error) =>
-          app.Actions.showError({
-            title: 'filterOptionsStore, onChangeFilterOptionsForObject: error preparing trigger:',
-            msg: error
-          })
+          reject('filterOptionsStore: error fetching filterOptions from localDb:', error)
         )
-    }
-  })
-  return filterOptionsStore
-}
+    })
+  },
+
+  onLoadFilterOptions(newItemsPassed) {
+    this.trigger({
+      filterOptions: null,
+      loading: true
+    })
+    // get existing filterOptions
+    this.getOptions()
+      .then((optionsFromPouch) => {
+        let filterOptions = optionsFromPouch
+        if (newItemsPassed) filterOptions = filterOptions.concat(buildFilterOptions(newItemsPassed))
+        const loading = false
+        this.trigger({ filterOptions, loading })
+      })
+      .catch((error) =>
+        app.Actions.showError({
+          title: 'filterOptionsStore, onLoadFilterOptions: error preparing trigger:',
+          msg: error
+        })
+      )
+  },
+
+  onChangeFilterOptionsForObject(object) {
+    const option = buildFilterOptionsFromObject(object)
+    let options = null
+    app.localDb.get('_local/filterOptions')
+      .then((doc) => {
+        // replace option with new
+        doc.filterOptions = doc.filterOptions.filter((op) =>
+          op.value !== object._id
+        )
+        doc.filterOptions.push(option)
+        options = doc.filterOptions
+        return app.localDb.put(doc)
+      })
+      .then(() => this.trigger({ options, loading: false }))
+      .catch((error) =>
+        app.Actions.showError({
+          title: 'filterOptionsStore, onChangeFilterOptionsForObject: error preparing trigger:',
+          msg: error
+        })
+      )
+  }
+})
