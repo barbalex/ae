3d54b0c67ef6bc43f1ac0c5dd452a5a2c4f94a95
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-29 21:16:57 +0200

    optimize styling

--- src/components/menu/resizeButton.js
+++ src/components/menu/resizeButton.js
@@ -9,9 +9,6 @@ import { Tooltip, OverlayTrigger, Glyphicon, Button } from 'react-bootstrap'
 const bodyElement = document.body
 
 const styles = StyleSheet.create({
-  resizeButtonRootDiv: {
-    
-  },
   button: {
     visibility: 'hidden',
     '@media screen and (min-width: 1001px)': {
@@ -19,7 +16,8 @@ const styles = StyleSheet.create({
     },
     ':focus': {
       outline: 'none'
-    }
+    },
+    height: 30
   }
 })
 
@@ -33,31 +31,29 @@ export default React.createClass({
 
   render() {
     return (
-      <div className={css(styles.resizeButtonRootDiv)}>
-        <OverlayTrigger
-          placement="left"
-          overlay={
-            <Tooltip
-              id="btnResizeTooltip"
-            >
-              {
-                hasClass(bodyElement, 'force-mobile') ?
-                'in zwei Spalten anzeigen' :
-                'ganze Breite nutzen'
-              }
-            </Tooltip>
-          }
-        >
-          <Button
-            id="btnResize"
-            bsSize="small"
-            onClick={this.resize}
-            className={css(styles.button)}
+      <OverlayTrigger
+        placement="left"
+        overlay={
+          <Tooltip
+            id="btnResizeTooltip"
           >
-            <Glyphicon glyph="resize-horizontal" />
-          </Button>
-        </OverlayTrigger>
-      </div>
+            {
+              hasClass(bodyElement, 'force-mobile') ?
+              'in zwei Spalten anzeigen' :
+              'ganze Breite nutzen'
+            }
+          </Tooltip>
+        }
+      >
+        <Button
+          id="btnResize"
+          bsSize="small"
+          onClick={this.resize}
+          className={css(styles.button)}
+        >
+          <Glyphicon glyph="resize-horizontal" />
+        </Button>
+      </OverlayTrigger>
     )
   }
 })
