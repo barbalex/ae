75660abc45bf0e18e7237522360580cdcde435e6
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-29 22:24:25 +0200

    optimize code

--- src/components/menu/groups/groupCheckbox.js
+++ src/components/menu/groups/groupCheckbox.js
@@ -3,19 +3,28 @@
 import app from 'ampersand-app'
 import React from 'react'
 import { Checkbox } from 'react-bootstrap'
+import { StyleSheet, css } from 'aphrodite'
+
+const styles = StyleSheet.create({
+  checkbox: {
+    marginTop: '9px'
+  },
+  label: {
+    marginLeft: '-5px'
+  }
+})
 
 const Group = ({ group }) =>
   <Checkbox
-    style={{
-      marginTop: '10px',
-      marginLeft: '-4px'
-    }}
+    className={css(styles.checkbox)}
     checked={false}
     onClick={() =>
       app.Actions.loadObject(group)
     }
   >
-    {group.replace('Macromycetes', 'Pilze')}
+    <span className={css(styles.label)}>
+      {group.replace('Macromycetes', 'Pilze')}
+    </span>
   </Checkbox>
 
 Group.displayName = 'Group'
