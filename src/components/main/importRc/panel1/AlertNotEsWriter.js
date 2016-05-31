92c466fd3d313ff7d29805d8d59105b714cdb053
Alexander Gabriel <alex@gabriel-software.ch>
2016-05-31 11:13:27 +0200

    optimize code

--- src/components/main/importRc/panel1/AlertNotEsWriter.js
+++ src/components/main/importRc/panel1/AlertNotEsWriter.js
@@ -1,25 +1,23 @@
 'use strict'
 
 import React from 'react'
-import { Alert } from 'react-bootstrap'
-
-const style = {
-  marginBottom: 5
-}
+import { Alert, FormGroup, ControlLabel } from 'react-bootstrap'
 
 const AlertNotEsWriter = () =>
-  <div className="form-group">
-    <Alert
-      className="feld"
-      bsStyle="danger"
-      style={style}
-    >
-      Sie besitzen keine Schreibrechte.<br />
-      Importieren kann nur, wer von einer beteiligten Organisation Schreibrechte erhält.<br />
-      Wenn Sie Beziehungen importieren möchten, wenden Sie sich bitte an
-      <a href="mailto:andreas.lienhard@bd.zh.ch"> die Fachstelle Naturschutz des Kantons Zürich</a>.
-    </Alert>
-  </div>
+  <FormGroup>
+    <ControlLabel style={{ display: 'block' }} />
+    <div style={{ width: '100%' }}>
+      <Alert
+        className="feld"
+        bsStyle="danger"
+      >
+        Sie besitzen keine Schreibrechte.<br />
+        Importieren kann nur, wer von einer beteiligten Organisation Schreibrechte erhält.<br />
+        Wenn Sie Beziehungen importieren möchten, wenden Sie sich bitte an
+        <a href="mailto:andreas.lienhard@bd.zh.ch"> die Fachstelle Naturschutz des Kantons Zürich</a>.
+      </Alert>
+    </div>
+  </FormGroup>
 
 AlertNotEsWriter.displayName = 'AlertNotEsWriter'
 
