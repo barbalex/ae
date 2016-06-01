61c8815d098a55646a7ba83164d2c5f7cc28550f
Alexander Gabriel <alex.barbalex@gmail.com>
2016-06-01 17:07:40 +0200

    style

--- src/components/main/importPc/panel1/InputDatenstand.js
+++ src/components/main/importPc/panel1/InputDatenstand.js
@@ -1,7 +1,13 @@
 'use strict'
 
 import React from 'react'
-import { OverlayTrigger, Popover, FormGroup, FormControl } from 'react-bootstrap'
+import { OverlayTrigger, Popover, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import { StyleSheet, css } from 'aphrodite'
+import labelWithPopover from '../../../../styles/labelWithPopover.js'
+
+const styles = StyleSheet.create({
+  label: labelWithPopover()
+})
 
 const popover = (
   <Popover
@@ -28,9 +34,11 @@ const InputDatenstand = ({
       placement="right"
       overlay={popover}
     >
-      <label className="control-label withPopover">
+      <ControlLabel
+        className={css(styles.label)}
+      >
         Datenstand
-      </label>
+      </ControlLabel>
     </OverlayTrigger>
     <FormControl
       componentClass="textarea"
--- src/components/main/importPc/panel1/InputImportiertVon.js
+++ src/components/main/importPc/panel1/InputImportiertVon.js
@@ -1,7 +1,13 @@
 'use strict'
 
 import React from 'react'
-import { OverlayTrigger, Popover, FormGroup, FormControl } from 'react-bootstrap'
+import { OverlayTrigger, Popover, FormGroup, FormControl, ControlLabel } from 'react-bootstrap'
+import { StyleSheet, css } from 'aphrodite'
+import labelWithPopover from '../../../../styles/labelWithPopover.js'
+
+const styles = StyleSheet.create({
+  label: labelWithPopover()
+})
 
 const popover = (
   <Popover
@@ -21,9 +27,11 @@ const InputImportiertVon = ({ importiertVon }) => (
       placement="right"
       overlay={popover}
     >
-      <label className="control-label withPopover">
+      <ControlLabel
+        className={css(styles.label)}
+      >
         importiert von
-      </label>
+      </ControlLabel>
     </OverlayTrigger>
     <FormControl
       type="text"
