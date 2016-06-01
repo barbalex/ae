7c0f0a0ce29195ad3517a1dc9a3f90b19f4caffa
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-01 11:15:46 +0200

    optimize styling

--- src/components/main/exportAlt/panel1/FieldsPCsPanel.js
+++ src/components/main/exportAlt/panel1/FieldsPCsPanel.js
@@ -3,6 +3,22 @@
 import React from 'react'
 import { Checkbox } from 'react-bootstrap'
 import { has, get } from 'lodash'
+import { StyleSheet, css } from 'aphrodite'
+
+const styles = StyleSheet.create({
+  fields: {
+    columnWidth: 450,
+    breakInside: 'avoid',
+    marginBottom: -8
+  },
+  cb: {
+    breakInside: 'avoid'
+  },
+  divAlleWaehlen: {
+    fontStyle: 'italic',
+    marginBottom: 5
+  }
+})
 
 const FieldsPCsPanel = ({
   pcFields,
@@ -30,6 +46,7 @@ const FieldsPCsPanel = ({
       <Checkbox
         key={fieldKey}
         checked={checked}
+        className={css(styles.cb)}
         onChange={(event) =>
           onChooseField(cNameKey, fNameKey, 'pc', event)
         }
@@ -43,11 +60,11 @@ const FieldsPCsPanel = ({
     const checked = collectionsWithAllChoosen.includes(cNameKey)
     alleField = (
       <div
-        className="felderspalte alleWaehlenCheckbox"
-        style={{ marginBottom: 5 }}
+        className={css(styles.fields, styles.divAlleWaehlen)}
       >
         <Checkbox
           checked={checked}
+          className={css(styles.cb)}
           onChange={(event) =>
             onChooseAllOfCollection(cNameKey, 'pc', event)
           }
@@ -61,8 +78,7 @@ const FieldsPCsPanel = ({
     <div>
       {alleField}
       <div
-        className="felderspalte"
-        style={{ marginBottom: -8 }}
+        className={css(styles.fields)}
       >
         {fields}
       </div>
