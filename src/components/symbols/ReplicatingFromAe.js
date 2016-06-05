dde07e57e62ca42975f41c1e59295cfbf0b61e88
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-05 15:25:29 +0200

    style inline

--- src/components/symbols/ReplicatingFromAe.js
+++ src/components/symbols/ReplicatingFromAe.js
@@ -2,18 +2,28 @@
 
 import React from 'react'
 import { OverlayTrigger, Tooltip, Glyphicon } from 'react-bootstrap'
+import { StyleSheet, css } from 'aphrodite'
 
 const ReplicatingFromAe = ({ replicatingFromAe, replicatingFromAeTime }) => {
-  let style = {}
   let tooltipText = `Repliziere von arteigenschaften.ch seit ${replicatingFromAeTime}`
+  let color = '#00AA00'
   if (replicatingFromAe === 'success') {
-    style.color = '#00AA00'
+    color = '#00AA00'
     tooltipText = `Zuletzt von arteigenschaften.ch repliziert: ${replicatingFromAeTime}`
   }
   if (replicatingFromAe === 'error') {
-    style.color = 'red'
+    color = 'red'
     tooltipText = `Replikation von arteigenschaften.ch gescheitert um: ${replicatingFromAeTime}`
   }
+
+  const styles = StyleSheet.create({
+    glyph: {
+      top: 2,
+      fontSize: 16,
+      paddingLeft: 5,
+      color
+    }
+  })
 
   return (
     <OverlayTrigger
@@ -29,9 +39,8 @@ const ReplicatingFromAe = ({ replicatingFromAe, replicatingFromAeTime }) => {
     >
       <Glyphicon
         id="replicateFromRemoteDb"
-        className="symbols symbol"
+        className={css(styles.glyph)}
         glyph="cloud-download"
-        style={style}
       />
     </OverlayTrigger>
   )
