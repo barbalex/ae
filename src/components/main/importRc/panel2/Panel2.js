e5a37dc4969b64c5f94f1c68332e8ea367bdcc64
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-05 15:25:44 +0200

    style inline

--- src/components/main/importRc/panel2/Panel2.js
+++ src/components/main/importRc/panel2/Panel2.js
@@ -2,10 +2,16 @@
 
 import React from 'react'
 import { FormGroup, FormControl } from 'react-bootstrap'
+import { StyleSheet, css } from 'aphrodite'
 import WellTechnAnforderungenAnDatei from './WellTechnAnforderungenAnDatei.js'
 import WellAnforderungenAnCsv from './WellAnforderungenAnCsv.js'
 import WellAnforderungenInhaltlich from './WellAnforderungenInhaltlich.js'
 import TablePreview from './TablePreview.js'
+import validateDiv from '../../../../styles/validateDiv.js'
+
+const styles = StyleSheet.create({
+  validateDiv: validateDiv()
+})
 
 const Panel2 = ({
   rcsToImport,
@@ -28,7 +34,7 @@ const Panel2 = ({
     </FormGroup>
     {
       !validRcsToImport &&
-      <div style={{ marginTop: '-4px' }} className="validateDiv">
+      <div style={{ marginTop: '-4px' }} className={css(styles.validateDiv)}>
         Bitte wählen Sie eine Datei
       </div>
     }
