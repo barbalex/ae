ae9b158fd1691d40dc864a1d5a27137b8bcf57d0
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-07 14:50:16 +0200

    work on store

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,8 +2,12 @@
 
 import { combineReducers } from 'redux'
 import { routerReducer as routing } from 'react-router-redux'
+import app from './app'
+import groups from './groups'
 
 const rootReducer = combineReducers({
+  app,
+  groups,
   routing
 })
 
