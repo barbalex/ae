0692065a875bd6e2af37a50e07e116366e1e1276
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 18:03:46 +0200

    work on store

--- src/components/main/organizations/removeRolesFromUser.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-export default (username, roles) =>
-  new Promise((resolve, reject) =>
-    fetch(`${window.location.protocol}//${window.location.hostname}:8000/removeuserroles/user/${username}/roles/${JSON.stringify(roles)}`)
-      .then(() => resolve(null))
-      .catch((error) => reject((error))
-    )
-  )
