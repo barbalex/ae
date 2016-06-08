dbd77bfe9c82966d1206c85fb43164a82904ee17
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 19:31:26 +0200

    work on store

--- src/router.js
+++ src/router.js
@@ -2,22 +2,22 @@ import React from 'react'
 import { Router, Route, browserHistory } from 'react-router'
 import configureStore from './configureStore.js'
 import { Provider } from 'react-redux'
-import Home from './containers/Home.js'
+import HomeCt from './components/HomeCt.js'
 
 export default () =>
   <Provider store={configureStore()}>
     <Router history={browserHistory}>
       <Route
         path="/"
-        component={Home}
+        component={HomeCt}
       />
       <Route
         path="*"
-        component={Home}
+        component={HomeCt}
       />
       <Route
         path="/**/*"
-        component={Home}
+        component={HomeCt}
       />
     </Router>
   </Provider>
