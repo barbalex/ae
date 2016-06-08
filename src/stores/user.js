7e9ba95504436b581050d3379e0ae02ae4d572ec
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 17:31:02 +0200

    work on store

--- src/stores/user.js
+++ src/stores/user.js
@@ -2,22 +2,21 @@
 
 import app from 'ampersand-app'
 import Reflux from 'reflux'
-import refreshUserRoles from '../modules/refreshUserRoles.js'
 
 export default (Actions) => Reflux.createStore({
   /*
    * contains email and roles of logged in user
-   * well, it is saved in pouch as local doc _local/login
+   * well, it is saved in pouch as local doc _local/user
    * and contains "logIn" bool which states if user needs to log in
-   * app.js sets default _local/login if not exists on app start
+   * app.js sets default _local/user if not exists on app start
    */
   listenables: Actions,
 
   getLogin() {
     return new Promise((resolve, reject) => {
-      app.localDb.get('_local/login')
+      app.localDb.get('_local/user')
         .then((doc) => {
-          refreshUserRoles(doc.email)
+          //refreshUserRoles(doc.email)
           const { logIn, email, roles } = doc
           this.trigger({ logIn, email, roles })
         })
@@ -28,7 +27,7 @@ export default (Actions) => Reflux.createStore({
   },
 
   onLogin({ logIn, email, roles }) {
-    app.localDb.get('_local/login')
+    app.localDb.get('_local/user')
       .then((doc) => {
         doc.logIn = logIn
         doc.email = email || undefined
