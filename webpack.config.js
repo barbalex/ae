74aeafd19c951bded977864a03a0c8cce57208cd
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 11:33:51 +0200

    optimize code

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,8 @@ const webpack = require('webpack')
 const config = getConfig({
   in: 'src/app.js',
   out: 'public',
-  isDev: process.env.NODE_ENV !== 'production' /*,
+  isDev: process.env.NODE_ENV !== 'production',
+  /*
   // here you can configure you local machine hostname
   // and share it with other devices on your network
   // hm. worked on computer but not on mobile
