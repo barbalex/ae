41c49ff353b95e29a1ed9e8a43837dca71146efa
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 18:07:44 +0200

    remove use strict

--- src/components/main/importRc/panel4/Panel4.js
+++ src/components/main/importRc/panel4/Panel4.js
@@ -1,5 +1,3 @@
-'use strict'
-
 import React from 'react'
 import { ProgressBar, Button, Glyphicon } from 'react-bootstrap'
 import ButtonDeleteRcInstances from './buttonDeleteRcInstances/ButtonDeleteRcInstances.js'
