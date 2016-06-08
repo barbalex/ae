d642c32fd1dbc6ed91a717b48870ad6c24045a0e
Alexander Gabriel <alex@gabriel-software.ch>
2016-06-08 19:19:08 +0200

    work on store

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,18 +4,22 @@ import Home from '../components/Home'
 import * as userActions from '../actions/user'
 import * as groupsActions from '../actions/groups'
 import * as appActions from '../actions/app'
+import * as errorActions from '../actions/errors'
 
 const actions = Object.assign(
   userActions,
   groupsActions,
-  appActions
+  appActions,
+  errorActions
 )
 
 function mapStateToProps(state) {
   const { user } = state.user
+  const { errors } = state.errors
 
   return {
-    user
+    user,
+    errors
   }
 }
 
