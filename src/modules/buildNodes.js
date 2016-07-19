e23c3f864c054a8c8dde760b5a9ab7ebe81089c9
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-19 15:42:46 +0200

    root node comes from api

--- src/modules/buildNodes.js
+++ src/modules/buildNodes.js
@@ -1,10 +1,8 @@
 const d3 = require('d3-hierarchy')
-import addRootNodeToNodes from './addRootNodeToNodes'
 
 export default (nodesData) => {
-  const nodesList = addRootNodeToNodes(nodesData)
   const root = d3.stratify()
     .id((n) => n.id)
-    .parentId((n) => n.parent_id)(nodesList)
+    .parentId((n) => n.parent_id)(nodesData)
   return root
 }
